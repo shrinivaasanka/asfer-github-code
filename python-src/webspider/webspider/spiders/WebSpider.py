173428925c6a945f095388041754152ab50ced90
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2015-12-14 16:50:34 +0530

    - Updated AsFer Design Document - New Interview Algorithm script with NetworkX+Matplotlib rendering and takes as input a randomly crawled HTML webpage(uses beautiful soup to rip off script and style tags and write only text in the HTML page) has been added - Above script also computes the graph theoretic connectivity of the RGO wordnet subgraph based on Menger's theorem - prints number of nodes required to disconnect the graph or equivalently number of node indpendent paths - logs and screenshots for the above have been added - WebSpider.py has been updated to crawl based on a crawling target parameter - Either a streaming website(twitter, streamed news etc.,) or a HTML webpage

--- python-src/webspider/webspider/spiders/WebSpider.py
+++ python-src/webspider/webspider/spiders/WebSpider.py
@@ -33,38 +33,47 @@ from injector import Module, provides, Injector, inject, singleton
 from scrapy.spiders import Spider
 from scrapy.selector import Selector
 from webspider.items import WebSpiderItem
+from bs4 import BeautifulSoup
 
 class WebSpider(Spider):
 	name = "webspider"
+	crawling = "HTML"
 	allowed_domains = "www.google.com"
 	#start_urls = ['https://www.google.com/search?hl=en&gl=in&tbm=nws&authuser=0&q=theoretical+computer+science&oq=theoretical+computer+science&gs_l=news-cc.3..43j43i53.30043.38477.0.38618.28.9.0.19.19.2.775.3607.2j1j1j0j1j2j2.9.0...0.0...1ac.1.mtwb_lgjbI4']
-	start_urls = ['https://www.google.com/search?hl=en&gl=in&tbm=nws&authuser=0&q=Chennai&oq=Chennai&gs_l=news-cc.3...1525.3020.0.3307.7.6.0.0.0.0.0.0..0.0...0.0...1ac.1.#q=Chennai&hl=en&gl=in&authuser=0&tbm=nws&tbs=sbd:1','https://www.google.com/search?hl=en&gl=in&tbm=nws&authuser=0&gl=in&authuser=0&tbs=sbd:1&tbm=nws&q=chennai+metropolitan+area+expansion','https://www.google.co.in/search?q=Chennai+metropolitan+area+expansion&oq=Chennai+metropolitan+area+expansion&aqs=chrome..69i57.12903j0j8&sourceid=chrome&es_sm=93&ie=UTF-8']
-	output=open("WebSpider.out","w")
+	#start_urls = ['https://www.google.com/search?hl=en&gl=in&tbm=nws&authuser=0&q=Chennai&oq=Chennai&gs_l=news-cc.3...1525.3020.0.3307.7.6.0.0.0.0.0.0..0.0...0.0...1ac.1.#q=Chennai&hl=en&gl=in&authuser=0&tbm=nws&tbs=sbd:1','https://www.google.com/search?hl=en&gl=in&tbm=nws&authuser=0&gl=in&authuser=0&tbs=sbd:1&tbm=nws&q=chennai+metropolitan+area+expansion','https://www.google.co.in/search?q=Chennai+metropolitan+area+expansion&oq=Chennai+metropolitan+area+expansion&aqs=chrome..69i57.12903j0j8&sourceid=chrome&es_sm=93&ie=UTF-8']
+	start_urls = ['http://tech.firstpost.com/news-analysis/apple-slashes-prices-of-bestseller-iphone-5s-now-available-for-rs-24999-in-india-291179.html']
+	output=open("WebSpider-"+crawling+".out","w")
 
 	def parse(self,response):
-		sys.path.insert(0,"/media/shrinivaasanka/0fc4d8a2-1c74-42b8-8099-9ef78d8c8ea2/home/kashrinivaasan/KrishnaiResearch_OpenSource/GitHub/asfer-github-code/python-src/backend")
-		import Abstract_DBBackend
-		import MySQL_DBBackend
-		import MySQL_Configuration
-		import MongoDB_DBBackend
-		import MongoDB_Configuration
-		from pymongo.collection import Collection
-		mysqldbobj=MySQL_DBBackend.MySQL_DBBackend()
-		mysqlconfigobj=MySQL_Configuration.MySQL_Configuration()
-		injector=Injector([mysqldbobj,mysqlconfigobj])
-		handler=injector.get(Abstract_DBBackend.Abstract_DBBackend)
-		#handler.execute_query("CREATE TABLE asfer_webspider(Description varchar(100))")
-		select = Selector(response)
-		links_list = select.xpath('//h3/a/@href').extract()
-		links_list2 = [re.search('q=(.*)&sa',n).group(1) for n in links_list]
-		desc_list=select.xpath('//h3/a/text()').extract()
-		items=[]
-        	for desc in desc_list:
-			item=WebSpiderItem()
-            		item['desc'] = desc 
-			items.append(item)
-			self.output.write(desc)
-			self.output.write("\n")
-			handler.execute_query("INSERT INTO asfer_webspider(Description) VALUES(\""+desc+"\")","MySQL")
-		handler.execute_query("SELECT * FROM asfer_webspider","MySQL")
-		return items
+		if self.crawling=="Streaming":
+			sys.path.insert(0,"/media/shrinivaasanka/0fc4d8a2-1c74-42b8-8099-9ef78d8c8ea2/home/kashrinivaasan/KrishnaiResearch_OpenSource/GitHub/asfer-github-code/python-src/backend")
+			import Abstract_DBBackend
+			import MySQL_DBBackend
+			import MySQL_Configuration
+			import MongoDB_DBBackend
+			import MongoDB_Configuration
+			from pymongo.collection import Collection
+			mysqldbobj=MySQL_DBBackend.MySQL_DBBackend()
+			mysqlconfigobj=MySQL_Configuration.MySQL_Configuration()
+			injector=Injector([mysqldbobj,mysqlconfigobj])
+			handler=injector.get(Abstract_DBBackend.Abstract_DBBackend)
+			#handler.execute_query("CREATE TABLE asfer_webspider(Description varchar(100))")
+			select = Selector(response)
+			links_list = select.xpath('//h3/a/@href').extract()
+			links_list2 = [re.search('q=(.*)&sa',n).group(1) for n in links_list]
+			desc_list=select.xpath('//h3/a/text()').extract()
+			items=[]
+			for desc in desc_list:
+				item=WebSpiderItem()
+				item['desc'] = desc
+				items.append(item)
+				self.output.write(desc)
+				self.output.write("\n")
+				handler.execute_query("INSERT INTO asfer_webspider(Description) VALUES(\""+desc+"\")","MySQL")
+			handler.execute_query("SELECT * FROM asfer_webspider","MySQL")
+			return items
+		if self.crawling=="HTML":
+			bsoup=BeautifulSoup(response.body)
+			for script in bsoup(["script","style"]):
+				script.extract()
+			self.output.write(bsoup.get_text().encode("utf-8"))
