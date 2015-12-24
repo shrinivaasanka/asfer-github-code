ad33f08289258c70f309fc7cd2ac4f4fe254b8bb
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2015-12-24 15:09:59 +0530

    - Updated AsFer Design Document -------------------------------------------------------------------------------------------------------------- Apache Spark MapReduce Parallel Computation of Interview Algorithm Recursive Gloss Overlap Graph Construction: -------------------------------------------------------------------------------------------------------------- - 2 python files for Spark MapReduce of Recursive Gloss Overlap graph construction has been added to repository - These two implement Interview Algorithm Recursive Gloss Overlap graph construction and Map-Reduce functions that parallellize each recursion step computing the gloss tokens from previous recursion level. This is the most important part of the algorithm which consumes lot of time in serial version. Apache Spark has been recently gaining importance over its plain Hadoop counterpart and is many times faster than Hadoop in benchmarks. - In this implementation, Resilient Distributed Dataset is the set of tokens in each recursion level and is parallelly computable in a huge Spark cluster. - For example if Spark cluster has 10000 nodes, and each level of recursion produces X number of gloss tokens for graph vertices, time complexity in Spark is O(X/10000) where as serial version is O(X) with negligible network overhead. Spark has in-memory datasets which minimizes network latency because of disk access. - There were lot of implementation issues to make the parallellism. Map and Reduce functions use namedtuples to return data. With more than one field in namedtuple, there are internal pickling issues in Py4J - Python4Java which PySpark internally invokes to get streamed socket bytes from Java side of the object wrapped as an iterable. This prevents returning multiple values - for example Synsets - in Map-Reduce functions. - So only tokens at a level are map-reduced and returned while the prevlevelsynsets (required for adding edges across vertices) are "pickled" with a properietary asfer_pickle_load() and asfer_pickle_dump() functions that circumvents the python and java vagaries in pickling. - With proprietary pickling and Map-Reduce functions, Recursive Gloss Overlap graph has been constructed in parallel. Screenshots for this has bee added in testlogs. - Proprietary pickling is done to a text file which is also added to repository. This file is truncated at the end of each recursion. - Subtlety here is that maximum number of tokens at a recursion level t = number_of_tokens_at_level_(t-1) * maximum_size_of_gloss_per_word(s) which grows as series 1 + s + s^2 + s^3 + ... + s^t. If size of the Spark cluster is O(s^t_max), each recursion step is O(1) and total time for all levels is O(t_max) which is per text document. This bound neglects optimization from overlaps and isomorphic nodes removal and is worst case upperbound. This implies size of cluster is independent of document size and depends only on depth of recursion. For ranking n documents this bound becomes O(n*t_max). For constant t_max this bound is O(n) - linear in number of documents and thus scalable. - Maximum size of gloss per word (s) is also an upper-boundable constant. With constant t_max , size of cluster O(s^t_max) is constant too.

---
+++ python-src/InterviewAlgorithm/InterviewAlgorithmWithIntrinisicMerit_SparkMapReducer.py
@@ -0,0 +1,143 @@
+#-------------------------------------------------------------------------------------------------------
+#ASFER - Software for Mining Large Datasets
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------------
+#Copyright (C):
+#Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
+#Ph: 9791499106, 9003082186
+#Krishna iResearch Open Source Products Profiles:
+#http://sourceforge.net/users/ka_shrinivaasan,
+#https://github.com/shrinivaasanka,
+#https://www.openhub.net/accounts/ka_shrinivaasan
+#Personal website(research): https://sites.google.com/site/kuja27/
+#emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com,
+#kashrinivaasan@live.com
+#--------------------------------------------------------------------------------------------------------
+
+#Apache Spark RDD MapReduce Transformations script for Recursive Gloss Overlap. Also creates a SparkSQL DataFrame temp table.
+#Following recursive graph construction is map-reduced in Spark
+                #for keyword in freqterms1:
+                #       #WSD - invokes Lesk's algorithm adapted to recursive gloss overlap- best_matching_synset()
+                #       disamb_synset = best_matching_synset(freqterms1, wn.synsets(keyword))
+                #       prevlevelsynsets = prevlevelsynsets + [disamb_synset]
+                #       if len(wn.synsets(keyword)) != 0:
+                #               disamb_synset_def = disamb_synset.definition()
+                #               tokens = nltk.word_tokenize(disamb_synset_def)
+                #               fdist_tokens = FreqDist(tokens)
+                #               tokensofthislevel = tokensofthislevel + ([w for w in fdist_tokens.keys() if w not in stopwords and w not in puncts and fdist_tokens.freq(w)])
+
+#Example pyspark RDD mapreduce code at: http://www.mccarroll.net/blog/pyspark2/
+
+from __future__ import division
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SQLContext, Row 
+import operator
+import pickle
+import sys
+
+import nltk
+from collections import defaultdict
+from nltk.corpus import wordnet as wn
+from nltk.book import *
+from nltk.corpus import stopwords
+from collections import namedtuple
+
+#rgo_object=namedtuple("rgo_object", "tokensatthislevel prevlevelsynsets")
+rgo_object=namedtuple("rgo_object", "tokensatthislevel")
+
+def asfer_pickle_dump(prevlevelsynsets,picklef):
+     for s in prevlevelsynsets:
+	picklef.write(repr(s)+",")
+
+def asfer_pickle_load(picklef):
+     line=picklef.readline()
+     synsets=[]
+     stringsynsets=line.split(",")
+     for s in stringsynsets:
+         s_synset_tokens=s.split("'")
+	 print "s_synset_tokens:",s_synset_tokens
+	 if len(s_synset_tokens) == 3:
+	     s_synset_word_tokens=s_synset_tokens[1].split(".")
+	     print "s_synset_word_tokens:",s_synset_word_tokens
+	     s_synsets=wn.synsets(s_synset_word_tokens[0])
+	     print "s_synsets:",s_synsets
+             synsets.append(s_synsets[0])	
+     print "asfer_pickle_load(): synsets=",synsets
+     return synsets
+
+def mapFunction(freqterms1):
+     prevlevelsynsets=[]
+     stopwords = nltk.corpus.stopwords.words('english')
+     stopwords = stopwords + [' ','or','and','who','he','she','whom','well','is','was','were','are','there','where','when','may', 'The', 'the', 'In','in','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+     puncts = [' ','.', '"', ',', '{', '}', '+', '-', '*', '/', '%', '&', '(', ')', '[', ']', '=', '@', '#', ':', '|', ';','\'s']
+     mapped_object=()
+     for keyword in freqterms1:
+       	 #WSD - invokes Lesk's algorithm adapted to recursive gloss overlap- best_matching_synset()
+         disamb_synset = best_matching_synset(freqterms1, wn.synsets(keyword))
+         prevlevelsynsets = prevlevelsynsets + [disamb_synset]
+         if len(wn.synsets(keyword)) != 0:
+      		         disamb_synset_def = disamb_synset.definition()
+      			 tokens = nltk.word_tokenize(disamb_synset_def)
+             		 fdist_tokens = FreqDist(tokens)
+         		 #fdist_tokens=[w for fdist_tokens.keys() if w not in stopwords and w not in puncts and fdist_tokens.freq(w)]
+	 		 #mapped_object=rgo_object(fdist_tokens.keys(),prevlevelsynsets)
+	 		 mapped_object=rgo_object(fdist_tokens.keys())
+     picklef=open("RecursiveGlossOverlap_MapReduce_Persisted.txt","ab")
+     asfer_pickle_dump(prevlevelsynsets,picklef)
+     return (1,mapped_object)
+ 
+def reduceFunction(mapped_object1,mapped_object2):
+    reduced_rgo_object=()
+    print "reduceFunction():mapped_object1: ",mapped_object1
+    print "reduceFunction():mapped_object2: ",mapped_object2
+    #reduced_rgo_object=rgo_object(mapped_object1.tokensatthislevel+mapped_object2.tokensatthislevel,mapped_object1.prevlevelsynsets+mapped_object2.prevlevelsynsets) 
+    if (not mapped_object1):	
+    	reduced_rgo_object=rgo_object(mapped_object2.tokensatthislevel) 
+    if (not mapped_object2):
+    	reduced_rgo_object=rgo_object(mapped_object1.tokensatthislevel) 
+    if (mapped_object1 and mapped_object2):
+    	reduced_rgo_object=rgo_object(mapped_object1.tokensatthislevel+mapped_object2.tokensatthislevel) 
+    print "reduceFunction():returns : ",reduced_rgo_object
+    return reduced_rgo_object
+
+#function - best_matching_synset()
+def best_matching_synset(doc_tokens, synsets):
+    maxmatch = -1
+    retset = []
+    for synset in synsets:
+     	 def_tokens = set(nltk.word_tokenize(synset.definition()))
+       	 intersection = def_tokens.intersection(doc_tokens)
+       	 if len(intersection) > maxmatch:
+       	          maxmatch = len(intersection)
+       	          retset = synset
+    return retset
+
+def Spark_MapReduce(level, wordsatthislevel):
+        spcon=SparkContext() 
+	paralleldata=spcon.parallelize(wordsatthislevel)
+	words=paralleldata.filter(lambda word: word)
+	k=words.map(mapFunction).reduceByKey(reduceFunction)
+
+	#dict_k=k.collect()
+	#s = sorted(dict_k.items(),key=operator.itemgetter(1), reverse=True)
+	#print "Spark MapReduce results:"
+	#print s
+	############################
+	sqlContext=SQLContext(spcon)
+	recursiveglossoverlap_schema=sqlContext.createDataFrame(k.collect())
+	recursiveglossoverlap_schema.registerTempTable("Interview_RecursiveGlossOverlap")
+	query_results=sqlContext.sql("SELECT * FROM Interview_RecursiveGlossOverlap")
+	dict_query_results=dict(query_results.collect())
+	print "SparkSQL DataFrame query results:"
+	print dict_query_results[1]
+	spcon.stop()
+	return dict_query_results[1]
