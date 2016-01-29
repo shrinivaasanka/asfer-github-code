5d7f32facfb50d33a12b0dc78eca088ae8f16ff5
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2016-01-29 15:13:40 +0530

    Python-C++-VIRGOKernel and Python-C-VIRGOKernel boost::python and cpython implementations: ------------------------------------------------------------------------------------------ - It is a known idiom that Linux Kernel and C++ are not compatible. - In this commit an important feature to invoke VIRGO Linux Kernel from userspace python libraries via two alternatives have been added. - In one alternative, C++ boost::python extensions have been added to encapsulate access to VIRGO memory system calls - virgo_malloc(), virgo_set(), virgo_get(), virgo_free(). Initial testing reveals that C++ and Kernel are not too incompatible and all the VIRGO memory system calls work well though initially there were some errors because of config issues. - In the other alternative, C Python extensions have been added that replicate boost::python extensions above in C - C Python with Linux kernel works exceedingly well compared to boost::python. - This functionality is required when there is a need to set kernel analytics configuration variables learnt by AsFer Machine Learning Code dynamically without re-reading /etc/virgo_kernel_analytics.conf. - This completes a major integration step of NeuronRain suite - request travel roundtrip to-and-fro top level machine-learning C++/python code and rock-bottom C linux kernel - bull tamed ;-). - This kind of python access to device drivers is available for Graphics Drivers already on linux (GPIO - for accessing device states) - logs for both C++ and C paths have been added in cpp_boost_python_extensions/ and cpython_extensions. - top level python scripts to access VIRGO kernel system calls have been added in both directories: 	CPython - python cpython_extensions/asferpythonextensions.py 	C++ Boost::Python - python cpp_boost_python_extensions/asferpythonextensions.py - .so, .o files with build commandlines(asferpythonextensions.build.out) for "python setup.py build" have been added in build lib and temp directories. - main implementations for C++ and C are in cpp_boost_python_extensions/asferpythonextensions.cpp and cpython_extensions/asferpythonextensions.c - Schematic Diagram: -------------------- 	AsFer Python -----> Boost::Python C++ Extension ------> VIRGO memory system calls --------> VIRGO Linux Kernel Memory Drivers 	/\												V 	 |												| 	 ---------------------------------------------<--------------------------------------------------
            AsFer Python -----> CPython Extensions ------> VIRGO memory system calls --------> VIRGO Linux Kernel Memory Drivers
    	 /\                                                                                             V
             |                                                                                              |
             ---------------------------------------------<--------------------------------------------------

---
+++ python-src/cpp_boost_python_extensions/asferpythonextensions.cpp
@@ -0,0 +1,87 @@
+/*
+-------------------------------------------------------------------------------------------------------
+#NEURONRAIN ASFER - Software for Mining Large Datasets
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------------
+#Copyleft(Copyright+):
+#Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
+#Ph: 9791499106, 9003082186
+#Krishna iResearch Open Source Products Profiles:
+#http://sourceforge.net/users/ka_shrinivaasan,
+#https://github.com/shrinivaasanka,
+#https://www.openhub.net/accounts/ka_shrinivaasan
+#Personal website(research): https://sites.google.com/site/kuja27/
+#emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com,
+#kashrinivaasan@live.com
+#--------------------------------------------------------------------------------------------------------
+*/
+
+#include <iostream>
+
+#include <stdio.h>
+#include <syscall.h>
+#include <unistd.h>
+#include <string.h>
+
+struct virgo_address
+{
+        int node_id;
+        struct hostport* hstprt;
+        void* addr;
+};
+
+struct hostport
+{
+        char* hostip;
+        int port;
+};
+
+using namespace std;
+
+void asfer_virgo_set_kernel_analytics(char* key, char* value)
+{
+	//cout<<"asfer_virgo_set_kernel_analytics():"<<"key:"<<key<<"; value:"<<value<<endl;	
+
+	// VIRGO malloc system calls to set the key-value pairs
+	
+	struct virgo_address* vaddr;
+	unsigned long virgo_unique_id=3250409088u;
+
+	// virgo_malloc 
+	syscall(360,100,&virgo_unique_id);
+	//printf("vuid malloc-ed : %lu \n",virgo_unique_id);
+
+	// virgo_set 
+	char set_data[256];
+	//strcpy(set_data,key);
+	//strcpy(set_data,":");
+	//strcpy(set_data,value);
+	//printf("vuid virgo_set() \n");
+	long set_ret=syscall(361,virgo_unique_id,"key1:value1");
+
+	// virgo_get
+	char get_data[256];
+	long get_ret=syscall(362,virgo_unique_id,get_data);
+	//printf("vuid virgo_get() : %s \n",get_data);
+
+	// virgo_free
+	// long free_ret=syscall(363,virgo_unique_id);
+}
+
+#include <boost/python/module.hpp>
+#include <boost/python/def.hpp>
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(asfer_virgo_pyext)
+{
+	def("asfer_virgo_set_kernel_analytics",asfer_virgo_set_kernel_analytics);
+}
---
+++ python-src/cpython_extensions/asferpythonextensions.c
@@ -0,0 +1,102 @@
+/*
+-------------------------------------------------------------------------------------------------------
+#NEURONRAIN ASFER - Software for Mining Large Datasets
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------------
+#Copyleft(Copyright+):
+#Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
+#Ph: 9791499106, 9003082186
+#Krishna iResearch Open Source Products Profiles:
+#http://sourceforge.net/users/ka_shrinivaasan,
+#https://github.com/shrinivaasanka,
+#https://www.openhub.net/accounts/ka_shrinivaasan
+#Personal website(research): https://sites.google.com/site/kuja27/
+#emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com,
+#kashrinivaasan@live.com
+#--------------------------------------------------------------------------------------------------------
+*/
+
+#include <Python.h>
+#include <stdio.h>
+#include <syscall.h>
+#include <unistd.h>
+#include <string.h>
+
+struct virgo_address
+{
+        int node_id;
+        struct hostport* hstprt;
+        void* addr;
+};
+
+struct hostport
+{
+        char* hostip;
+        int port;
+};
+
+static PyObject* asfer_virgo_set_kernel_analytics(PyObject* self, PyObject* args)
+{
+	/* VIRGO malloc system calls to set the key-value pairs */
+	
+	char key[30];
+	char value[30];	
+	/*
+	if (!PyArg_ParseTuple(args, "s", &key))
+		return NULL;
+	if (!PyArg_ParseTuple(args, "s", &value))
+		return NULL;
+	
+	printf("asfer_virgo_set_kernel_analytics(): %s %s \n",key,value);
+	*/
+	
+	struct virgo_address* vaddr;
+	unsigned long virgo_unique_id=3250409088u;
+
+	/* virgo_malloc */
+	syscall(360,100,&virgo_unique_id);
+
+	/* virgo_set */
+	/*
+	char set_data[256];
+	strcpy(set_data,key);
+	strcpy(set_data,":");
+	strcpy(set_data,value);
+	printf("asfer_virgo_set_kernel_analytics(): set_data: %s\n",set_data);
+	long set_ret=syscall(361,virgo_unique_id,set_data);
+	*/
+	long set_ret=syscall(361,virgo_unique_id,"key1:value1");
+
+	/* virgo_get */
+	char get_data[256];
+	long get_ret=syscall(362,virgo_unique_id,get_data);
+
+	/* virgo_free */
+	long free_ret=syscall(363,virgo_unique_id); 
+	Py_RETURN_NONE;
+}
+
+static char py_add_doc[]="asfer_virgo_set_kernel_analytics";
+
+static PyMethodDef AsFer_VIRGO_Methods[] =
+{
+	{"asfer_virgo_set_kernel_analytics", asfer_virgo_set_kernel_analytics, METH_VARARGS, py_add_doc},
+	{NULL, NULL, 0, NULL}
+};
+
+/*PyMODINIT_FUNC*/
+void 
+initasfer_virgo_pyext(void)
+{
+	PyObject* mod;
+	mod=Py_InitModule("asfer_virgo_pyext", AsFer_VIRGO_Methods);
+}
