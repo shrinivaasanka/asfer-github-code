3927ebf33f7a03d00ef5cea0a4c68854824016ea
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2016-03-14 17:44:36 +0530

    - Some bugs resolved - Added one more example with no pattern - Convolution is computed for all 3 bitmap examples - Final neuron layer now is a function of each point in all maxpooling layers - The existence of pattern is identified by the final output of each of 10 neurons - Patterns 0 and 3 have a greater neural value than no pattern. Gradation of neural value indicates intensity of pattern. - Above is a very fundamental pattern recognition for 2 dimensional data. Sophisticated deconvolution is explained in http://arxiv.org/abs/1311.2901 which reverse engineers pooling layers with Unpooling. - 3 logs for this commit have been included in testlogs/ - random weighting has been removed.

--- python-src/DeepLearning_ConvolutionNetwork.py
+++ python-src/DeepLearning_ConvolutionNetwork.py
@@ -134,7 +134,7 @@ class DeepLearningConvolution(object):
 			convolution_map_index+=1
 		return self.convolution_map	
 
-	def max_pooling(self,pooling_width):
+	def max_pooling(self,pooling_slidewindow_width):
 		for convolution_map_index in xrange(3):
 			row=col=0
 			k=l=0
@@ -142,11 +142,11 @@ class DeepLearningConvolution(object):
 		   		while col < len(self.convolution_map[convolution_map_index])-1:
 					maximum = self.find_maximum(self.convolution_map[convolution_map_index][row][col], self.convolution_map[convolution_map_index][row+1][col], self.convolution_map[convolution_map_index][row][col+1], self.convolution_map[convolution_map_index][row+1][col+1])
 					self.max_pooling_map[convolution_map_index][k][l]=maximum
-					col=col+pooling_width
+					col=col+pooling_slidewindow_width
 					l=l+1
 		   		col=0
 		   		l=0
-		   		row=row+pooling_width
+				row=row+pooling_slidewindow_width
 		   		k=k+1
 		return self.max_pooling_map
 			
@@ -160,71 +160,127 @@ class DeepLearningConvolution(object):
 		   maximum=d
 		return maximum	
 
-	def infer_from_max_pooling(self,max_pooling_map,pooling_neuron_weight,pool_width):
-		weighted_sum=[]
+	#Connects all points in all max pooling layers into a single neural activation function
+	def infer_from_max_pooling(self,max_pooling_map,pooling_neuron_weight,maxpool_map_width):
+		weighted_sum=0.0
 		self.max_pooling_inference=[]
-		for convolution_map_index in xrange(3):
-			weighted_sum.append(0.0)
-		for convolution_map_index in xrange(3):
-			for p in xrange(pool_width):
-		   		for q in xrange(pool_width):
-					weighted_sum[convolution_map_index] = weighted_sum[convolution_map_index] + max_pooling_map[convolution_map_index][p][q]*(pooling_neuron_weight*self.weight[convolution_map_index][p][q])
-			self.max_pooling_inference.append(self.sigmoid(weighted_sum[convolution_map_index]+self.bias))
+		for p in xrange(maxpool_map_width):
+			for q in xrange(maxpool_map_width):
+				for convolution_map_index in xrange(3):
+					weighted_sum = weighted_sum + max_pooling_map[convolution_map_index][p][q]*(self.weight[convolution_map_index][p][q])
+		self.max_pooling_inference.append(self.sigmoid(weighted_sum+self.bias))
 		return self.max_pooling_inference
                        
 			
 
-#An example input picture with '0' inscribed as set of 1s
-input_bitmap1=[[0,0,0,0,0,0,0,0,0,0],
- 	      [0,0,0,0,0,0,0,0,0,0],
-	      [0,1,1,1,1,1,1,1,1,0],
-	      [0,1,1,1,1,1,1,1,1,0],
-	      [0,1,1,1,0,0,1,1,1,0],
-	      [0,1,1,1,0,0,1,1,1,0],
-	      [0,1,1,1,1,1,1,1,1,0],
-	      [0,1,1,1,1,1,1,1,1,0],
-	      [0,0,0,0,0,0,0,0,0,0],
-	      [0,0,0,0,0,0,0,0,0,0]]
+#An example input picture bitmap with '0' inscribed as set of 1s
+input_bitmap1=[[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,1,1,1,1,0,0,0,1,1,1,1,0,0],
+	[0,1,1,1,1,0,0,0,1,1,1,1,0,0],
+	[0,1,1,1,1,0,0,0,1,1,1,1,0,0],
+	[0,1,1,1,1,0,0,0,1,1,1,1,0,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
-#An example input picture with '3' inscribed as set of 1s
+#An example input picture bitmap with '3' inscribed as set of 1s
 input_bitmap2=[[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
- 	      [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-	      [0,0,1,1,1,1,1,1,1,1,1,0,0,0],
-      	      [0,0,1,1,1,1,1,1,1,1,1,0,0,0],
-	      [0,0,1,0,0,0,0,0,1,1,0,0,0,0],
-	      [0,0,0,0,0,0,0,1,1,0,0,0,0,0],
-	      [0,0,0,0,0,0,1,1,1,1,1,0,0,0],
-	      [0,0,0,0,0,0,1,1,1,1,1,0,0,0],
-	      [0,0,0,0,0,0,0,0,0,1,1,0,0,0],
-	      [0,0,1,0,0,0,0,0,0,1,1,0,0,0],
-	      [0,0,1,1,1,1,1,1,1,1,1,0,0,0],
-	      [0,0,1,1,1,1,1,1,1,1,1,0,0,0],
-	      [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-	      [0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,1,1,1,1,1,1,1,1,1,1,1,0],
+	[0,0,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,0,1,1,1,1,1,1,1,1,1,0,0,0],
+	[0,0,0,0,0,0,0,1,1,1,0,0,0,0],
+	[0,0,0,0,0,1,1,1,1,1,1,0,0,0],
+	[0,0,0,0,0,1,1,1,1,1,1,0,0,0],
+	[0,0,0,0,0,1,1,1,1,1,1,0,0,0],
+	[0,0,1,0,0,0,0,0,1,1,1,0,0,0],
+	[0,0,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,0,1,1,1,1,1,1,1,1,1,1,1,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
-dlc=DeepLearningConvolution(input_bitmap2)
+#An example input picture bitmap with no patterns
+input_bitmap3=[[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+dlc1=DeepLearningConvolution(input_bitmap1)
+dlc2=DeepLearningConvolution(input_bitmap2)
+dlc3=DeepLearningConvolution(input_bitmap3)
 
 #maximum stride is 5
 convolution_stride=2
-conv_map1=dlc.convolution(convolution_stride)
+conv_map1=dlc1.convolution(convolution_stride)
+conv_map2=dlc2.convolution(convolution_stride)
+conv_map3=dlc3.convolution(convolution_stride)
 
-#maximum pool width is 5
-pool_width=2
-pool_map1=dlc.max_pooling(pool_width)
+#maximum pool sliding window width is 5
+pool_slidewindow_width=2
+pool_map1=dlc1.max_pooling(pool_slidewindow_width)
+pool_map2=dlc2.max_pooling(pool_slidewindow_width)
+pool_map3=dlc3.max_pooling(pool_slidewindow_width)
 
 print "##########################################"
 print "Set of Convolution Maps"
 print "##########################################"
+print "Example 1:"
+print "###########"
 pprint.pprint(conv_map1)
+print "###########"
+print "Example 2:"
+print "###########"
+pprint.pprint(conv_map2)
+print "###########"
+print "Example 3:"
+print "###########"
+pprint.pprint(conv_map3)
 print "##########################################"
 print "Max Pooling Map"
 print "##########################################"
-print pool_map1
+print "Example 1:"
+print "###########"
+pprint.pprint(pool_map1)
+print "###########"
+print "Example 2:"
+print "###########"
+pprint.pprint(pool_map2)
+print "###########"
+print "Example 3:"
+print "###########"
+pprint.pprint(pool_map3)
 print "####################################################################################################"
 print "Final layer that connects all neurons in max pooling map into set of 10 neurons"
 print "####################################################################################################"
+maxpool_map_width=5
 for w in xrange(10):
 	print "###########################################################################################"
 	print "Inference from Max Pooling Layer - Neuron ",w
 	print "###########################################################################################"
-	print dlc.infer_from_max_pooling(pool_map1,float(random.randint(1,w+1))/float(w+1),pool_width)
+	print "Example 1:"
+	print "###########"
+	pprint.pprint(dlc1.infer_from_max_pooling(pool_map1,float(random.randint(1,w+1))/float(w+1),maxpool_map_width))
+	print "###########"
+	print "Example 2:"
+	print "###########"
+	pprint.pprint(dlc2.infer_from_max_pooling(pool_map2,float(random.randint(1,w+1))/float(w+1),maxpool_map_width))
+	print "###########"
+	print "Example 3:"
+	print "###########"
+	pprint.pprint(dlc3.infer_from_max_pooling(pool_map3,float(random.randint(1,w+1))/float(w+1),maxpool_map_width))
