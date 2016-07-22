3352650e59221a63b91fa3988f945d505f9cf915
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2016-07-22 15:28:09 +0530

    -------------------------------------------------------------------------------------------- Software Analytics - Cyclomatic Complexity from SATURN .dot graphs -------------------------------------------------------------------------------------------- New Python-Spark implementation that reads the SATURN program analyzer generated .dot graph files processes them with Spark MapReduce to find the number of edges and vertices in the graph represented by .dot file for each snippet. From this Cyclomatic Complexity is calculated (which is Edges - Vertices + 2) -  a standard Function Point Estimator for code complexity. There were some JVM crashes frequently while starting up spark-submit logs for which are also committed in testlogs along with the two successful Spark computed Cyclomatic Complexity measures for two .dot files. These .dot files are sourced from VIRGO Linux saturn program analysis kernel driver.

---
+++ python-src/software_analytics/CyclomaticComplexitySparkMapReducer.py
@@ -0,0 +1,59 @@
+#-------------------------------------------------------------------------------------------------------
+#NEURONRAIN ASFER - Software for Mining Large Datasets
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------------
+#Copyleft (Copyright+):
+#Srinivasan Kannan
+#(also known as: Shrinivaasan Kannan, Shrinivas Kannan)
+#Ph: 9791499106, 9003082186
+#Krishna iResearch Open Source Products Profiles:
+#http://sourceforge.net/users/ka_shrinivaasan,
+#https://github.com/shrinivaasanka,
+#https://www.openhub.net/accounts/ka_shrinivaasan
+#Personal website(research): https://sites.google.com/site/kuja27/
+#emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com,
+#kashrinivaasan@live.com
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+#Apache Spark RDD MapReduce Transformations script for parsing the number of edges and vertices 
+#in SATURN generated .dot graph files for program control flow. Cyclomatic Complexity which is a 
+#basic function point estimator for complexity of a software is determined as Edges - Vertices + 2. 
+
+#Example pyspark RDD mapreduce code at: http://www.mccarroll.net/blog/pyspark2/
+
+from pyspark import SparkContext, SparkConf
+
+def reduceFunction(value1,value2):
+     return value1+value2
+
+def mapFunction(line):
+     for i in line.split():
+	   return (i,1)
+
+if __name__=="__main__":
+	spcon=SparkContext() 
+	input_saturn_dot_files=['/media/shrinivaasanka/0fc4d8a2-1c74-42b8-8099-9ef78d8c8ea2/home/kashrinivaasan/KrishnaiResearch_OpenSource/GitHub/virgo-linux-github-code/linux-kernel-extensions/drivers/virgo/saturn_program_analysis/saturn_program_analysis_trees/cfg_read_virgo_kernel_analytics_config.dot','/media/shrinivaasanka/0fc4d8a2-1c74-42b8-8099-9ef78d8c8ea2/home/kashrinivaasan/KrishnaiResearch_OpenSource/GitHub/virgo-linux-github-code/linux-kernel-extensions/drivers/virgo/saturn_program_analysis/saturn_program_analysis_trees/memory_skbuff_h_skb_header_pointer_cfg.dot']
+	for dot_file in input_saturn_dot_files:
+		input=open(dot_file,'r')
+		paralleldata=spcon.parallelize(input.readlines())
+		node_edge_lines=paralleldata.filter(lambda nodeedge: "node" in nodeedge)
+		k1=node_edge_lines.map(mapFunction).reduceByKey(reduceFunction)
+		edgesplusvertices=k1.collect()
+
+		edge_lines=paralleldata.filter(lambda nodeedge: "->" in nodeedge)
+		k2=edge_lines.map(mapFunction).reduceByKey(reduceFunction)
+		vertices=k2.collect()
+		print "Edges + Vertices:",edgesplusvertices
+		print "Vertices:",vertices
+		print "Cyclomatic Complexity: E-V+2 = ",abs(len(edgesplusvertices)-2*len(vertices)+2)
+		#l=k.map(lambda src: src).reduce(lambda x,y: x if (x[1] > y[1]) else y)
+		#print l
