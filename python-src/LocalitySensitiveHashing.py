260537c26d1c75be8790b4cbafd373fa190b5b96
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2016-09-16 16:31:25 +0530

    ------------------------------------------------------------------------------------------------------------------------------------ Locality Sensitive Hashing Implementation ------------------------------------------------------------------------------------------------------------------------------------ 1.This commit implements locality sensitive hashing in python by wrapping defaultdict with hashing and distance measures. 2.Locality Sensitive Hashing is useful for clustering similar strings or text documents into same bucket and is thus an unsupervised classifier and an inverted index too. 3.In this implementation, very basic LSH is done by having replicated hashtables and hashing a document to each of these hashtables with a random polynomial hash function which itself is aggregation of random monomials. 4.For a query string, random hash function is again computed and buckets from all hashtables corresponding to this hash value is returned as nearest neighbour set. 5.Each of these buckets are sieved to find the closest neighbour for that hashtable. 6.Sorting the nearest neighbours for all hash tables yields a ranking of documents which are in the vicinity of the query string. 7.The input strings are read from a text file LocalitySensitiveHashing.txt 8.Logs with hashtable dumps and nearest neighbour rankings are in testlogs/LocalitySensitiveHashing.out.16September2016

---
+++ python-src/LocalitySensitiveHashing.py
@@ -0,0 +1,125 @@
+#-------------------------------------------------------------------------------------------------------
+#NEURONRAIN ASFER - Software for Mining Large Datasets
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------------
+#Copyleft (Copyright+):
+#Srinivasan Kannan
+#(also known as: Shrinivaasan Kannan, Shrinivas Kannan)
+#Ph: 9791499106, 9003082186
+#Krishna iResearch Open Source Products Profiles:
+#http://sourceforge.net/users/ka_shrinivaasan,
+#https://github.com/shrinivaasanka,
+#https://www.openhub.net/accounts/ka_shrinivaasan
+#Personal website(research): https://sites.google.com/site/kuja27/
+#emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com,
+#kashrinivaasan@live.com
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+#Locality Sensitive Hashing Implementation - Nearest Neighbour Search:
+#---------------------------------------------------------------------
+#A random hash function polynomial of very high degree is created by concatenation of monomials. There are multiple replicated hash tables. 
+#Each key is hashed onto each of the hash tables by randomly chosen hash function polynomial. For nearest neighbour query q, all hashtables
+#are lookedup with random hash polynomial function,and bucket contents are returned. Shortest distance neighbour amongst them is returned as
+#nearest neighbour.
+
+from collections import defaultdict
+import jellyfish
+import random
+import sys
+import operator
+
+class LSH(object):
+	def __init__(self,size=10,number_of_tables=50,number_of_functions=50):
+		self.hashfunctions=[]
+		for k in range(1,number_of_functions):
+			self.hashfunctions.append("x**" + str(k) + "*" + str(k))
+		self.hashtables=[]
+		for i in range(1,number_of_tables):
+			self.hashtables.append(defaultdict(list))
+		self.size=size
+		self.randomhashfunctions=[]
+		reload(sys)
+		sys.setdefaultencoding("utf8")
+		self.nearest_neighbours={}
+
+	def add(self,element):
+		for t in range(0,len(self.hashtables)):
+			self.add_to_table(element,t)
+
+	def query_nearest_neighbours(self,e):
+		for t in range(0,len(self.hashtables)):
+			sumhash = 0
+			for c in e:
+				#print "sumhash=",sumhash
+				sumhash += ord(c) * (t+1)
+			x=sumhash
+			hashcode=eval(self.randomhashfunctions[t])
+			#print "Nearest Neighbours in hashtable ",t,":",self.hashtables[t][hashcode % self.size]	
+			nearest_neighbour,nearest_distance=self.find_nearest_neighbour(e,self.hashtables[t][hashcode % self.size])
+			print "Nearest Neighbour in hashtable ",t," = ",nearest_neighbour,nearest_distance	
+			self.nearest_neighbours[nearest_distance]=nearest_neighbour
+                sorted_nearest_neighbours = sorted(self.nearest_neighbours.items(),key=operator.itemgetter(0), reverse=False)
+		print "Top Nearest Neighbours Ranked for Query - ",e,":"
+		print sorted_nearest_neighbours
+
+	def find_nearest_neighbour(self, e, neighbours):
+		minneighbour=""
+		mindistance=100000000000.0
+		d=0	
+		for n in neighbours:
+            		d=jellyfish.levenshtein_distance(unicode(e),unicode(n))
+			if d < mindistance:
+				mindistance=d
+				minneighbour=n
+		return minneighbour,mindistance 
+	
+
+	def add_to_table(self,element,t):
+		hashcode=self.hash(element,t) 	
+		self.hashtables[t][hashcode % self.size].append(element)
+
+	def hash(self,e,t):
+		randomhashfunction=""
+		sumhash=0
+		for i in range(1,10):
+			randomhashfunction=randomhashfunction + "+" + self.hashfunctions[random.randint(0,len(self.hashfunctions)-1)]
+		randomhashfunction=randomhashfunction[1:]
+		for c in e:
+			#print "sumhash=",sumhash
+			sumhash += ord(c) * (t+1)
+		x=sumhash
+		#print "x=",x
+		#print "randomhashfunction=",randomhashfunction
+		hashcode=eval(randomhashfunction)
+		#print "hashcode:",hashcode
+		self.randomhashfunctions.append(randomhashfunction)
+		return hashcode
+
+	def dump_contents(self):
+		for t in range(1,len(self.hashtables)):
+			print "##################################################"
+			print "Slot -------------------- Buckets"
+			print "##################################################"
+			for k,v in self.hashtables[t].iteritems():
+				print k,"<==============>",	
+				for i in v:
+					print i,"#",
+				print 
+
+if __name__=="__main__":
+	lsh=LSH()
+	crawled=open("LocalitySensitiveHashing.txt","r")
+	for sentence in crawled:
+		lsh.add(sentence)
+	lsh.query_nearest_neighbours("Sentence6 long")
+	lsh.dump_contents()
+
