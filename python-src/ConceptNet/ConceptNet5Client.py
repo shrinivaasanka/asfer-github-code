874eb2a3060f142715191b3aa792d335462bc348
K.Srinivasan <ka.shrinivaasan@gmail.com>
2017-07-07 12:10:34 +0530

    ---------------------------------------------------------------------------------------------------------------- ConceptNet 5.4 Python RESTful API implementation - lookup, search and association ---------------------------------------------------------------------------------------------------------------- (*) This commit implements the RESTful Python requests HTTP API for querying ConceptNet 5.4 dataset (*) Three functions for looking up a concept, searching a concept and finding similar associated concepts have been implemented with 3 endpoints (as per the documentation in https://github.com/commonsense/conceptnet5/wiki/API/2349f2bbd1d7fb726b3bbdc14cbeb18f0a40ef18) (*) ConceptNet is quite different from WordNet in representation as JSON dictionary as against graph in WordNet. (*) ConceptNet 5.4 endpoints have been invoked instead of 5. (*) If necessary ConceptNet can replace all WordNet invocations in a later point in time. But such a replacement is non-trivial and would probably require almost all Recursive Gloss Overlap related code to be rewrittern from scratch. But that depends on how ConceptNet 5 weighs against WordNet in measuring semantic meaningfulness.

---
+++ python-src/ConceptNet/ConceptNet5Client.py
@@ -0,0 +1,64 @@
+#-------------------------------------------------------------------------------------------------------
+#NEURONRAIN ASFER - Software for Mining Large Datasets
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------------
+#Copyleft (Copyright+):
+#Srinivasan Kannan
+#(also known as: Shrinivaasan Kannan, Shrinivas Kannan)
+#Ph: 9791499106, 9003082186
+#Krishna iResearch Open Source Products Profiles:
+#http://sourceforge.net/users/ka_shrinivaasan,
+#https://github.com/shrinivaasanka,
+#https://www.openhub.net/accounts/ka_shrinivaasan
+#Personal website(research): https://sites.google.com/site/kuja27/
+#emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com,
+#kashrinivaasan@live.com
+#---------------------------------------------------------------------------------------------------------
+
+import requests
+import pprint
+
+class ConceptNet5Client:
+	def __init__(self):
+		print "Init of ConceptNet Client"
+
+	def query_association(self,concept1,concept2):
+		conceptjson=requests.get("http://conceptnet5.media.mit.edu/data/5.4/assoc/c/en/"+concept1+"?filter=/c/en/"+concept2).json()
+		#conceptjson=requests.get("http://conceptnet5.media.mit.edu/data/5.4/assoc/c/en/"+concept1).json()
+		return conceptjson
+
+	def query_search(self,concept):
+		conceptjson=requests.get("http://conceptnet5.media.mit.edu/data/5.4/search?end=/c/en/"+concept).json()
+		return conceptjson
+
+	def query_lookup(self,concept):
+		conceptjson=requests.get("http://conceptnet5.media.mit.edu/data/5.4/c/en/"+concept).json()
+		return conceptjson
+
+if __name__=="__main__":
+	conceptnet = ConceptNet5Client()
+	print "========================================"
+	print "Association"
+	print "========================================"
+	conceptjson=conceptnet.query_association("chennai","marina")
+	pprint.pprint(conceptjson)
+	print "========================================"
+	print "Search"
+	print "========================================"
+	conceptjson=conceptnet.query_search("chennai")
+	pprint.pprint(conceptjson)
+	print "========================================"
+	print "Lookup"
+	print "========================================"
+	conceptjson=conceptnet.query_lookup("chennai")
+	pprint.pprint(conceptjson)
+		
