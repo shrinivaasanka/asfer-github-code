39accc3cce916118b1a3bfbeb56f3d6b0b0b7085
K.Srinivasan <ka.shrinivaasan@gmail.com>
2017-11-09 17:24:46 +0530

    ------------------------------------------------------------------------------------------------------ Support Vector Machines implementation - based on CVXPY ------------------------------------------------------------------------------------------------------ (*) New Support Vector Machines python implementation is committed to NeuronRain AsFer repository (*) This minimizes and objective function 1/2*||w|| subject to constraint ||WX+b|| >= 1 which labels a point +1 or -1 on either side of a decision separating hyperplane WX+b for weight vector W and bias b (Reference: Machine Learning - Ethem Alpaydin - Chapter 13 - Kernel Machines) (*) Optimization is solved by CVXPY DCCP Convex-Concave program (*) logs for this have been added to testlogs/ (*) Present NeuronRain AsFer SVMRetriever.cpp depends on third-party SVMlight opensource software. With this new implementation, references to SVMlight are to be phased out.

---
+++ python-src/SupportVectorMachines.py
@@ -0,0 +1,90 @@
+#-------------------------------------------------------------------------------------------------------
+#NEURONRAIN ASFER - Software for Mining Large Datasets
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------------
+#Copyleft (Copyright+):
+#Srinivasan Kannan
+#(also known as: Shrinivaasan Kannan, Shrinivas Kannan)
+#Ph: 9791499106, 9003082186
+#Krishna iResearch Open Source Products Profiles:
+#http://sourceforge.net/users/ka_shrinivaasan,
+#https://github.com/shrinivaasanka,
+#https://www.openhub.net/accounts/ka_shrinivaasan
+#Personal website(research): https://sites.google.com/site/kuja27/
+#emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com,
+#kashrinivaasan@live.com
+#--------------------------------------------------------------------------------------------------------
+
+import math
+from cvxpy import * 
+import numpy
+import dccp
+import cvxopt
+
+class SupportVectorMachines(object):
+	def distance_from_separating_hyperplane(self,no_of_tuple,no_of_weights,tple=None):
+		weights=Variable(no_of_weights,1)
+		if tple==None:
+			tuple=numpy.random.rand(1,no_of_tuple)
+		else:
+			tuple=numpy.array(tple)	
+		print "weights:",weights
+		print "tuple:",tuple
+
+		bias=10.0
+		svm_function = 0.0 
+
+		for i in xrange(no_of_weights):
+			svm_function += weights[i,0]
+
+		objective=Minimize(abs(svm_function)*0.5)
+		print "============================================"
+		print "Objective Function"
+		print "============================================"
+		print objective
+
+		constraint=0.0
+		constraints=[]
+		for i in xrange(no_of_weights):
+			constraint += weights[i,0]*tuple[0,i] 
+		constraint += bias
+		constraints.append(abs(constraint) >= 1)
+		
+		print "============================================"
+		print "Constraints"
+		print "============================================"
+		print constraints
+
+		problem=Problem(objective,constraints)
+		print "====================================="
+		print "Installed Solvers:"
+		print "====================================="
+		print installed_solvers()
+		print "Is Problem DCCP:",dccp.is_dccp(problem)
+		print "====================================="
+		print "CVXPY args:"
+		print "====================================="
+		result=problem.solve(solver=SCS,verbose=True,method='dccp')
+		print "====================================="
+		print "Problem value:"
+		print "====================================="
+		print problem.value
+		print "====================================="
+		print "Result:"
+		print "====================================="
+		print result
+
+if __name__=="__main__":
+	cvx=SupportVectorMachines()
+	point=[45,3,3,4,4,2,6,2,56,1]
+	#cvx.distance_from_separating_hyperplane(10,10,point)
+	cvx.distance_from_separating_hyperplane(10,10)
