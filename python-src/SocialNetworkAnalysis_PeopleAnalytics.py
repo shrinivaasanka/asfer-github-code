bb4fff401f99164f2248f2ff69f20c3d2df12180
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2018-11-09 18:59:57 +0530

    ---------------------------------------------------------------------------------------------------------- 613. (FEATURE) People Analytics - HR Analytics Implementation and Least Energy Intrinsic Merit of a social profile - 9 November 2018 - related to 443, 572 ---------------------------------------------------------------------------------------------------------- (*) People Analytics is the generalized version of HR Analytics for analyzing any social profile vertex. (*) This commit adds SocialNetworkAnalysis_PeopleAnalytics.py python implementation to repository which at present analyzes LinkedIn profiles and computes:         (*) Graph Tensor Neuron Network Intrinsic Merit of the profile text with no field parsing - this treats the social profile as an en masse text with no delineations and applies Recursive Lambda Function Growth algorithm to create lambda function composition tree of the resume text. Core classification and Centrality measures throw some interesting insights:                 (*) Core number 4 identifies the class of the social profile                 (*) Maximum merit random walk of the lambda composition tree approximately guesses the purport of the text in the social profile                 (*) Betweenness and Closeness Centralities estimate the top-ranked classes accurately compared to Degree and PageRank Centralities         (*) Log Normal Fitness of the social profile after parsing the total work experience and academic stints of the profile - for linkedin this inverse log normal least energy fitness is 1/(logE + logV) with no Wealth estimates - Wealth is caused by E(Education) and V(Work/Valour) as far as professional profiles are concerned. Lowest Value of Least Energy Fitness implies Highest Fitness. Parsing is done by parse_profile() function which implements a switch design pattern - stint polymorphs from work to academic tenures.         (*) Log Normal Experiential Intrinsic Fitness of the social profile (by applying mistake bound learning differential equation for recursive mistake correction tree described earlier) - log normalized because of huge exponents (*) Because of limitations due to Updated LinkedIn Scraping Policy (https://www.forbes.com/sites/forbestechcouncil/2017/09/20/linkedin-vs-hiq-ruling-casts-a-long-shadow-over-the-tech-industry/#19f535f35e6c), this is only implemented as a non-crawled example for LinkedIn profile of self: https://www.linkedin.com/comm/in/srinivasan-kannan-608a671 (pdf and text version of pdf by pdf2txt) (*) datetime python package is used to compute timedelta(s) and a regex matcher isdaterange() function has been implemented for parsing date ranges in the profile:         (*) Work Experience Date Ranges have the Regex: <Month> <Year> - <Month> <Year>         (*) Academic Date Ranges have the Regex: <Year> - <Year> and months are implicit (*) This implementation does not assume a Web Crawler and Social Profile Datasource and only requires a file input (datasource, file type and file name are passed in as arguments to parse_profile()) - optionally, crawled social profiles can be invoked as file arguments to parse_profile(). Presently only linkedin datasource, text file types are supported. PDF parsing has been implemented by PyPDF2 but there are IndirectObject related errors.

---
+++ python-src/SocialNetworkAnalysis_PeopleAnalytics.py
@@ -0,0 +1,133 @@
+#-------------------------------------------------------------------------------------------------------
+#NEURONRAIN ASFER - Software for Mining Large Datasets
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------------
+#K.Srinivasan
+#NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+#Personal website(research): https://sites.google.com/site/kuja27/
+#--------------------------------------------------------------------------------------------------------
+
+import PyPDF2
+from RecursiveLambdaFunctionGrowth import RecursiveLambdaFunctionGrowth
+from datetime import datetime
+import sys
+import math
+
+class HRAnalytics(object):
+	def __init__(self):
+		self.profile_text=[]
+		self.work_experience=[]
+		self.academics=[]
+		self.total_work_experience=0
+		self.total_academics=0
+
+	def parse_profile(self, datasource, type, social_profile):
+		if type=="pdf":
+			self.file=open(social_profile,"rb")
+			file_reader=PyPDF2.PdfFileReader(self.file)
+			num_pages=file_reader.numPages
+			for p in xrange(num_pages):
+				page_object=file_reader.getPage(p)
+				page_contents=page_object.getContents()
+				for c in page_contents:
+					print c.getObject()
+		if datasource=="linkedin" and type=="text":
+			self.file=open(social_profile,"r")
+			profile_text=self.file.read()
+			stints=[]
+			self.file=open(social_profile,"r")
+			for l in self.file.readlines():
+				ltok=l.split()
+				#print "ltok:",ltok
+				if "Experience" in ltok:
+					print "Profile"
+					stints=self.work_experience
+				if "Education" in ltok:
+					print "Education"
+					stints=self.academics
+				if self.isdaterange(l):
+					stints.append(l.strip())
+			print "Work Experience:",self.work_experience
+			print "Academics:",self.academics	
+			return profile_text
+
+	def isdaterange(self,l):
+		months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+		monthdict={'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
+		try:
+			ltok=l.split("-")
+			starttok=[]
+			endtok=[]
+			start=""
+			end=""
+			if len(ltok) > 1:
+				start=ltok[0]
+				starttok=start.split()
+				end=ltok[1]
+				endtok=end.split()
+			if len(ltok) == 1:
+				start=ltok[0]
+				starttok=start.split()
+			if len(starttok) > 1:
+				if (starttok[0] in months and endtok[0] in months) or end == "Present":
+					startmonth=monthdict[starttok[0]]
+					startyear=int(starttok[1])
+					endmonth=monthdict[endtok[0]]
+					endyear=int(endtok[1])
+					startdate=datetime(startyear,startmonth,1)
+					enddate=datetime(endyear,endmonth,1)
+					print "startdate:",startdate
+					print "enddate:",enddate
+					tdelta=startdate-enddate
+					self.total_work_experience += abs(tdelta.total_seconds())
+					return True
+			if len(starttok) == 1:
+				if (int(starttok[0]) and int(endtok[0])) or end == "Present":
+					startyear=int(starttok[0])
+					endyear=int(endtok[0])
+					startdate=datetime(startyear,1,1)
+					enddate=datetime(endyear,1,1)
+					tdelta=startdate-enddate
+					self.total_academics += abs(tdelta.total_seconds())
+					print "startdate:",startdate
+					print "enddate:",enddate
+					return True	
+			return False
+		except Exception:
+			print sys.exc_info() 
+			return False
+			
+	def rlfg_intrinsic_merit(self, profile_contents):
+		rlfg=RecursiveLambdaFunctionGrowth()
+		rlfg.grow_lambda_function3(profile_contents)
+
+	def least_energy_intrinsic_merit(self):
+		print "Total work experience timedeltas:", self.total_work_experience
+		print "Total academics:",self.total_academics
+		self.log_normal_least_energy_intrinsic_merit = 1.0/float(math.log(self.total_work_experience) + math.log(self.total_academics)) 
+		print "Inverse Log Normal Least Energy Intrinsic Merit (low values imply high merit):",self.log_normal_least_energy_intrinsic_merit
+
+	def experiential_intrinsic_merit(self):
+		#E = M*e^(kMt)		
+		M=self.log_normal_least_energy_intrinsic_merit
+		t=self.total_work_experience
+		k=1
+		self.experiential_intrinsic_merit=math.log(M) + float(k*M*t)
+		print "Log Normal Experiential Intrinsic Merit:",self.experiential_intrinsic_merit	
+
+if __name__=="__main__":
+	hranal=HRAnalytics()
+	#hranal.parse_profile("linkedin","pdf","testlogs/ProfileLinkedIn_KSrinivasan.pdf")
+	profile_text=hranal.parse_profile("linkedin","text","testlogs/ProfileLinkedIn_KSrinivasan.txt")
+	hranal.rlfg_intrinsic_merit(profile_text)
+	hranal.least_energy_intrinsic_merit()
+	hranal.experiential_intrinsic_merit()
