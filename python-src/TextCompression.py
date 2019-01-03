106526984fb2e6bf95610522c75768b19760e038
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2019-01-03 22:32:16 +0530

    --------------------------------------------------------------------------------------------------------------- 627. (FEATURE) Vowelless Text (De)Compression - simplified likelydict MLE for HMM - 3 January 2019 --------------------------------------------------------------------------------------------------------------- 1. TextCompression.py has been classified to new class VowellessText and all functions have been made its members. 2. One time Likelyhood Dictionary computation of prefixes,suffixes and substrings has been shifted to WordSubstringProbabilities.py - wordlikelydict() and wordprefixsuffixsubstringsprobdist() 3. All precomputed prefix and suffix priors are initialized in TextCompression.py __init__() 4. Costly loop in HiddenMarkovModel_MLE() of TextCompression.py has been done away with and instead wordlikelydict() has been changed to choose between costly exhaustive search and substringsdict list comprehension. 5. Hardcoded priors for HMM have been removed and replaced by previous likelihood computation. 6. Logs for this have been committed to testlogs/TextCompression.log.3January2019 7. likelydict is created on demand once per compressed word than earlier heavy initialization for all possible likelihoods. 8. Regular Expressions are used in list comprehension for matching strings - "_" are treated as alphabets in lower or upper case. 9. For Exhaustive Search. following line is crucial which computes the conditional probability of a prefix for a suffix: 		likelydict[k3[:-1]+k3[len(k3)-1]+k4[1:]] = v3 * v4 10. The else clause, generalizes it and applies precomputed frequency probabilities of concatenated substrings circumventing the following conditional probability: 		likelydict[substring1 + substring2 + ... + substringN] = Pr(substring1) * Pr(substring2) * ... * Pr(substringN) by: 		likelydict[substring1 + substring2 + ... + substringN] = Pr(substring1 + substring2 + ... + substringN) 11. Both should be almost equal if substring random variables are independent and identically distributed. 12. An example English text from https://www.valmikiramayan.net/utf8/yuddha/sarga128/yuddha_128_frame.htm - Verse 120 has been compressed and decompressed. 13. Decompressed text has almost more than 50% accuracy and most priors are equal misleading the decipherment. 14. Previous HMM based probabilistic decompression can be compared to List decoding Berlekamp-Welch implementation which maps text to a polynomial

--- python-src/TextCompression.py
+++ python-src/TextCompression.py
@@ -18,29 +18,78 @@
 
 import WordSubstringProbabilities
 
-vowels = ['A','E','I','O','U','a','e','i','o','u']
+class VowellessText:
+	def __init__(self):
+		self.vowels = ['A','E','I','O','U','a','e','i','o','u']
+		print 			"#####################################################################################################"
+		#This is only a sample MLE using Forward probabilities.
+		#Implementing for all words requires precomputed constant read-only hashtable of approximately 
+		#500000 substring-probabilites entries or more.	
 
-def removevowels(w):
-	wcomp=""
-	for x in w:
-		if x in vowels:
-			wcomp = wcomp + "_"
-		else:
-			wcomp = wcomp + x
-	return wcomp
+		#Prior probabilites for "tha", "the", "thi", "tho", "thu" prefixes
+		#Probabilities_prefixes={"tha":0.4, "the":0.3, "thi":0.1, "tho":0.1, "thu":0.1, "ca":0.3, "ce":0.1, "ci":0.1, "co":0.4, "cu":0.1}
 
-def compute_maximumlikelihood(comp, suggestions):
-	maxlikely=""
-	maxlikelyprob=0.0
-	for x in suggestions:
-		#Bayesian 
-		#probability=prob(x,comp)	
-		#if probability > maxlikelyprob:
-		#	maxlikelyprob = probability
-		#	maxlikely=x
-		maxlikely = HiddenMarkovModel_MLE(comp)
-	print "comp=",comp,"; maxlikely=",maxlikely
-	return maxlikely 
+		#Prior probabilites for "at", "et", "it", "ot", "ut" suffixes
+		#Probabilities_suffixes={"at":0.3, "et":0.2, "it":0.1, "ot":0.2, "ut":0.2, "as":0.1, "es":0.2, "is":0.4, "os":0.2, "us":0.1}
+		prefixsuffixprob=WordSubstringProbabilities.wordprefixsuffixsubstringsprobdist()
+		self.Probabilities_prefixes=prefixsuffixprob[0]
+		self.Probabilities_suffixes=prefixsuffixprob[1]
+		#self.likelydict=prefixsuffixprob[2]
+		#print "prefixsuffixprob=",prefixsuffixprob
+
+		maxk1=""
+		maxv1=-1.0
+		maxk2=""
+		maxv2=-1.0
+
+		for k1,v1 in self.Probabilities_prefixes.items():
+			if maxv1 < v1:
+				maxv1 = v1
+				maxk1 = k1	
+				#maxvowel = maxk1[len(maxk1)-1]
+
+		for k2,v2 in self.Probabilities_suffixes.items():
+			if maxv2 < v2:
+				maxv2 = v2
+				maxk2 = k2 
+	
+		self.matchinglikelydict={}
+	
+		#Precomputed read-only Hidden Markov Model Forward probabilities product priors hashtable
+		#This is for all substrings in English dictionary and in its full size could have 500000+ entries
+		#and has to be read from disk storage
+		#for k3,v3 in (self.Probabilities_prefixes.items()):
+		#	for k4,v4 in (self.Probabilities_suffixes.items()):
+		#		#print "k3=",k3,"; k4=",k4
+		#		if len(k3) > 0 and len(k4) > 0 and (k3[len(k3)-1] == k4[0]):
+		#			self.likelydict[k3[:-1]+k3[len(k3)-1]+k4[1:]] = v3 * v4 
+
+		#for k5, v5 in self.Probabilities_suffixes.items():
+		#	self.likelydict[k5]=v5
+		#for k6, v6 in self.Probabilities_prefixes.items():
+		#	self.likelydict[k6]=v6
+
+	def removevowels(self, w):
+		wcomp=""
+		for x in w:
+			if x in self.vowels:
+				wcomp = wcomp + "_"
+			else:
+				wcomp = wcomp + x
+		return wcomp
+
+	def compute_maximumlikelihood(self, comp, suggestions=None):
+		maxlikely=""
+		maxlikelyprob=0.0
+		#for x in suggestions:
+			#Bayesian 
+			#probability=prob(x,comp)	
+			#if probability > maxlikelyprob:
+			#	maxlikelyprob = probability
+			#	maxlikely=x
+		maxlikely = self.HiddenMarkovModel_MLE(comp)
+		print "comp=",comp,"; maxlikely=",maxlikely
+		return maxlikely 
 
 #def prob(x,comp):
 #	if len(x) > len(comp):
@@ -59,167 +108,131 @@ def compute_maximumlikelihood(comp, suggestions):
 #			return 0.0
 #
 
-def HiddenMarkovModel_MLE(comp):
-	print "#####################################################################################################"
-	#This is only a sample MLE using Forward probabilities.
-	#Implementing for all words requires precomputed constant read-only hashtable of approximately 
-	#500000 substring-probabilites entries or more.	
+	def HiddenMarkovModel_MLE(self,comp):
+		#For loop that matches a compressed word to likely candidate words by left-to-right substring comparison
+		#between compressed word's substrings and the dictionary entries and populates a matching word dictionary
+		compsubstrings=comp.split("_")
+		print "compsubstrings: ",compsubstrings
 
-	#Prior probabilites for "tha", "the", "thi", "tho", "thu" prefixes
-	Probabilities_prefixes={"tha":0.4, "the":0.3, "thi":0.1, "tho":0.1, "thu":0.1, "ca":0.3, "ce":0.1, "ci":0.1, "co":0.4, "cu":0.1}
+		#start=0
+		#i=0
+		#compsubstrindex=-2
+		#match=False
+		#for k7, v7 in self.likelydict.items():
+		#	print "k7=",k7,"; v7=",v7
+		#	while start < len(k7) and i < len(compsubstrings):
+		#		print "finding substring :",compsubstrings[i]," in : ",k7
+		#		compsubstrindex = k7.find(compsubstrings[i],start)
+		#		print "compsubstrindex =",compsubstrindex
+		#		if compsubstrindex == -1:
+		#			match = False
+		#			break
+		#		start = start + len(compsubstrings[i]) 
+		#		i=i+1
+		#	print "compsubstrindex=",compsubstrindex,"; i=",i
+		#	if compsubstrindex > -1 and len(comp)==len(k7):
+		#		match=True
+		#		print "matching; adding to matching likely dict k7 =",k7,"; v7=",v7
+		#		self.matchinglikelydict[k7] = v7
+		#	match = False
+		#	compsubstrindex = -2
+		#	start=0
+		#	i=0
+		#likelydictwords = WordSubstringProbabilities.wordlikelydict(compsubstrings)
+		#likelydictmatchwords = []
+		#self.matchinglikelydict={}
+		#for compsubstring in compsubstrings:
+		#	likelydictmatchwordsfragment = [x for x in likelydictwords if x.find(compsubstring) != -1]
+		#	likelydictmatchwords = likelydictmatchwords + likelydictmatchwordsfragment 
+		#for likelymatch in likelydictmatchwords:
+		#	self.matchinglikelydict[likelymatch] = likelydictwords[likelymatch]
+		self.matchinglikelydict = WordSubstringProbabilities.wordlikelydict(comp)
 
-	#Prior probabilites for "at", "et", "it", "ot", "ut" suffixes
-	Probabilities_suffixes={"at":0.3, "et":0.2, "it":0.1, "ot":0.2, "ut":0.2, "as":0.1, "es":0.2, "is":0.4, "os":0.2, "us":0.1}
-	prefixsuffixprob=WordSubstringProbabilities.wordprefixsuffixprobdist()
-	#Probabilities_prefixes=prefixsuffixprob[0]
-	#Probabilities_suffixes=prefixsuffixprob[1]
-	#print "prefixsuffixprob=",prefixsuffixprob
-
-	maxk1=""
-	maxv1=-1.0
-	maxk2=""
-	maxv2=-1.0
-
-	for k1,v1 in Probabilities_prefixes.items():
-		if maxv1 < v1:
-			maxv1 = v1
-			maxk1 = k1	
-			#maxvowel = maxk1[len(maxk1)-1]
-
-	for k2,v2 in Probabilities_suffixes.items():
-		if maxv2 < v2:
-			maxv2 = v2
-			maxk2 = k2 
+		#Returns the word with maximum likelihood probability
+		#Word returned depends on the HMM priors in the hashtable
+		print "matchinglikelydict=",self.matchinglikelydict	
+		maxlikelyretprob=-1.0
+		maxlikelyret=""	
+		for k7, v7 in self.matchinglikelydict.items():
+			if maxlikelyretprob < v7:
+				maxlikelyret = k7	
+				maxlikelyretprob = v7
 	
-	likelydict={}
-	matchinglikelydict={}
+		print "maxlikelyretprob=",maxlikelyretprob, "; maxlikelyret = ",maxlikelyret	
+		print self.matchinglikelydict
+		return maxlikelyret
+
+
+	def prob(self,x,comp):
+		#Only suggestions with vowels as additional letters are sifted.
+		#two pointers in the suggested word and compressed word get incremented
+		#and 1 is returned if the compressed word's alphabets are contained in
+		#suggested word preserving order from left to right
+		print "=============================================="
+		setx=set(x)
+		setcomp=set(comp)
+		#if setcomp.issubset(setx):
+		setdiff=setx.difference(setcomp)
+		setdiffconso = [y for y in setdiff if y not in vowels]
 	
-	#Precomputed read-only Hidden Markov Model Forward probabilities product priors hashtable
-	#This is for all substrings in English dictionary and in its full size could have 500000+ entries
-	#and has to be read from disk storage
-	for k3,v3 in Probabilities_prefixes.items():
-		for k4,v4 in Probabilities_suffixes.items():
-			#print "k3=",k3,"; k4=",k4
-			if len(k3) > 0 and len(k4) > 0 and (k3[len(k3)-1] == k4[0]):
-				likelydict[k3[:-1]+k3[len(k3)-1]+k4[1:]] = v3 * v4 
+		if len(setdiffconso)==0 and len(x) > len(comp):
+			xptr=0
+			compptr=0
+			print "x=",x,"; comp=",comp
+			while xptr < len(x) and compptr < len(comp):
+				while xptr < len(x) and comp[compptr] != x[xptr]:
+					print "x[xptr]=",x[xptr],"; comp[compptr]=",comp[compptr]
+					xptr=xptr+1
+				compptr=compptr+1
+			#Both pointers should have reached the end of the respective words
+			if xptr == len(x) and compptr == len(comp):
+				print "x = ",x,";comp = ",comp,"; return 1"
+				return 1.0
+			else:
+				return 0.0
+		
 
-	for k5, v5 in Probabilities_suffixes.items():
-		likelydict[k5]=v5
-	for k6, v6 in Probabilities_prefixes.items():
-		likelydict[k6]=v6
+if __name__=="__main__":	
+	import enchant
+	f=open("texttocompress.txt","r")
+	fcomp = open("compressedtext.txt","w")
+	fdecomp = open("decompressedtext.txt","w")	
 
-	compsubstrings=comp.split("_")
-	print compsubstrings
+	d = enchant.Dict("en_US")
+	vowellesstxt=VowellessText()
+	origfilesize=0
+	compfilesize=0
+	compbits=0
 
-	#For loop that matches a compressed word to likely candidate words by left-to-right substring comparison
-	#between compressed word's substrings and the dictionary entries and populates a matching word dictionary
-	start=0
-	i=0
-	compsubstrindex=-2
-	match=False
-	for k7, v7 in likelydict.items():
-		print "k7=",k7,"; v7=",v7
-		while start < len(k7) and i < len(compsubstrings):
-			print "finding substring :",compsubstrings[i]," in : ",k7
-			compsubstrindex = k7.find(compsubstrings[i],start)
-			print "compsubstrindex =",compsubstrindex
-			if compsubstrindex == -1:
-				match = False
-				break
-			start = start + len(compsubstrings[i]) 
-			i=i+1
-		print "compsubstrindex=",compsubstrindex,"; i=",i
-		if compsubstrindex > -1 and len(comp)==len(k7):
-			match=True
-			print "matching; adding to matching likely dict k7 =",k7,"; v7=",v7
-			matchinglikelydict[k7] = v7
-		match = False
-		compsubstrindex = -2
-		start=0
-		i=0
-
-	#Returns the word with maximum likelihood probability
-	#Word returned depends on the HMM priors in the hashtable
-	print "matchinglikelydict=",matchinglikelydict	
-	maxlikelyretprob=-1.0
-	maxlikelyret=""	
-	for k7, v7 in matchinglikelydict.items():
-		if maxlikelyretprob < v7:
-			maxlikelyret = k7	
-			maxlikelyretprob = v7
-
-	print "maxlikelyretprob=",maxlikelyretprob, "; maxlikelyret = ",maxlikelyret
-	print matchinglikelydict
-	return maxlikelyret
-
-
-def prob(x,comp):
-	#Only suggestions with vowels as additional letters are sifted.
-	#two pointers in the suggested word and compressed word get incremented
-	#and 1 is returned if the compressed word's alphabets are contained in
-	#suggested word preserving order from left to right
-	print "=============================================="
-	setx=set(x)
-	setcomp=set(comp)
-	#if setcomp.issubset(setx):
-	setdiff=setx.difference(setcomp)
-	setdiffconso = [y for y in setdiff if y not in vowels]
-
-	if len(setdiffconso)==0 and len(x) > len(comp):
-		xptr=0
-		compptr=0
-		print "x=",x,"; comp=",comp
-		while xptr < len(x) and compptr < len(comp):
-			while xptr < len(x) and comp[compptr] != x[xptr]:
-				print "x[xptr]=",x[xptr],"; comp[compptr]=",comp[compptr]
-				xptr=xptr+1
-			compptr=compptr+1
-		#Both pointers should have reached the end of the respective words
-		if xptr == len(x) and compptr == len(comp):
-			print "x = ",x,";comp = ",comp,"; return 1"
-			return 1.0
-		else:
-			return 0.0
-	
-
-import enchant
-f=open("texttocompress.txt","r")
-fcomp = open("compressedtext.txt","w")
-fdecomp = open("decompressedtext.txt","w")
-
-d = enchant.Dict("en_US")
-origfilesize=0
-compfilesize=0
-compbits=0
-
-wcompcomp=""
-#Compression
-print "Compression by vowel removal:"
-for i in f:
-	wcomp = removevowels(i)
-	#remove _ in compressed word
-	for k in wcomp:
-		if k != '_':
-			wcompcomp =  wcompcomp + k
-		else:
-			compbits = compbits+1
-	print "wcompcomp = ",wcompcomp
-	origfilesize = origfilesize + len(i)
-	compfilesize = compfilesize + len(wcompcomp)
-	for n in wcomp:
-		fcomp.write(n)	
-	print "Compression ratio = ", 100*(origfilesize-compfilesize-0.125*compbits)/origfilesize, "%"
+	wcompcomp=""
+	#Compression
+	print "Compression by vowel removal:"
+	for i in f:
+		wcomp = vowellesstxt.removevowels(i)
+		#remove _ in compressed word
+		for k in wcomp:
+			if k != '_':
+				wcompcomp =  wcompcomp + k
+			else:
+				compbits = compbits+1
+		print "wcompcomp = ",wcompcomp
+		origfilesize = origfilesize + len(i)
+		compfilesize = compfilesize + len(wcompcomp)
+		for n in wcomp:
+			fcomp.write(n)	
+		print "Compression ratio = ", 100*(origfilesize-compfilesize-0.125*compbits)/origfilesize, "%"
 	fcomp.close()
+	
+	fcomp = open("compressedtext.txt","r")
+	fcompstr = fcomp.read()
 
-fcomp = open("compressedtext.txt","r")
-fcompstr = fcomp.read()
-
-#Decompression
-print "Decompression using PyEnchant Spellcheck suggest() function:"
-for i in fcompstr.split():
-	suggestions=d.suggest(i)
-	print i, suggestions
-	#midpoint=len(suggestions)
-	maximumlikelihoodword=compute_maximumlikelihood(i,suggestions)	
-	fdecomp.write(maximumlikelihoodword)
-	fdecomp.write(" ")
+	#Decompression
+	print "Decompression using PyEnchant Spellcheck suggest() function:"
+	for i in fcompstr.split():
+		suggestions=d.suggest(i)
+		print i, suggestions
+		#midpoint=len(suggestions)
+		#maximumlikelihoodword=vowellesstxt.compute_maximumlikelihood(i,suggestions)	
+		maximumlikelihoodword=vowellesstxt.compute_maximumlikelihood(i)
+		fdecomp.write(maximumlikelihoodword)
+		fdecomp.write(" ")
--- python-src/WordSubstringProbabilities.py
+++ python-src/WordSubstringProbabilities.py
@@ -18,42 +18,79 @@
 
 import nltk
 import json
+import re
 from nltk.probability import FreqDist	
 englishdict=open("Dictionary.txt","r")
+englishdicttxt=englishdict.readlines()
 prefixes=[]
 suffixes=[]
+wordlist=[]
+substrings=[]
 prefixdict={}
 suffixdict={}
+substringsdict={}
 prefixprobdict={}
 suffixprobdict={}
+substringsprobdict={}
+ExhaustiveSearch=False
 
-def computeprefixessuffixes(w):
+def computeprefixessuffixessubstrings(w):
 	for n in xrange(len(w)-1):
 		prefixes.append(w[:n].lower())
 	for n in xrange(len(w)-1):
 		suffixes.append(w[n:].lower())
+	for x in xrange(len(w)-1):
+		for y in xrange(len(w)-1):
+			substrings.append(w[x:y].lower()) 
 
-def wordprefixsuffixprobdist():
-	for w in englishdict:
+def wordprefixsuffixsubstringsprobdist():
+	for w in englishdicttxt:
 		wtok=w.split()
 		if len(wtok) > 0:		
-			computeprefixessuffixes(wtok[0])
+			computeprefixessuffixessubstrings(wtok[0])
+			wordlist.append(wtok[0])
 	#prefixf=open("WordPrefixesProbabilities.txt","w")
 	#suffixf=open("WordSuffixesProbabilities.txt","w")
 	prefixdict=FreqDist(prefixes)
 	suffixdict=FreqDist(suffixes)
+	substringsdict=FreqDist(suffixes)
 	totalprefixes=sum(prefixdict.values())
 	totalsuffixes=sum(suffixdict.values())
+	totalsubstrings=sum(substringsdict.values())
 	for pk,pv in zip(prefixdict.keys(), prefixdict.values()):
 		prefixprobdict[pk] = float(pv)/float(totalprefixes)
 	for pk,pv in zip(suffixdict.keys(), suffixdict.values()):
 		suffixprobdict[pk] = float(pv)/float(totalsuffixes)
+	for pk,pv in zip(substringsdict.keys(), substringsdict.values()):
+		substringsprobdict[pk] = float(pv)/float(totalsubstrings)
 	#json.dump(prefixprobdict,prefixf)
 	#json.dump(suffixprobdict,suffixf)
-	print "prefix probabilities:",prefixprobdict
-	print "suffix probabilities:",suffixprobdict
-	return (prefixprobdict, suffixprobdict)
-	
+	#print "prefix probabilities:",prefixprobdict
+	#print "suffix probabilities:",suffixprobdict
+	return (prefixprobdict, suffixprobdict, substringsprobdict)
+
+def wordlikelydict(comp):
+	likelydict={}
+	if ExhaustiveSearch==True:
+		for k3,v3 in (prefixprobdict.items()):
+			for k4,v4 in (suffixprobdict.items()):
+					#print "k3=",k3,"; k4=",k4
+					if len(k3) > 0 and len(k4) > 0 and (k3[len(k3)-1] == k4[0]):
+						likelydict[k3[:-1]+k3[len(k3)-1]+k4[1:]] = v3 * v4 
+			for k5, v5 in suffixprobdict.items():
+				likelydict[k5]=v5
+			for k6, v6 in prefixprobdict.items():
+				likelydict[k6]=v6
+		print likelydict
+	else:
+		compwordregex = "^"+comp.replace("_","[a-zA-Z]")+"$"
+		print "compwordregex:",compwordregex
+		matchstrings=set([(k,v) for k,v in substringsprobdict.items() if re.search(compwordregex,k) is not None])
+		for p in matchstrings:
+			likelydict[p[0]]=substringsprobdict[p[0]]
+		print "likelydict for ",comp,":",likelydict
+		return likelydict
+
 if __name__=="__main__":
 	wordprefixsuffixprobdist()
 	
