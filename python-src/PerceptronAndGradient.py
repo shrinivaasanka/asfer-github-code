3fe24a60725529b27c6eed6a632001fea9970721
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2019-08-04 15:46:09 +0530

    -------------------------------------------------------------------------------------------------------------------------------------------- 695. (FEATURE) Gradient Ascent and Descent - Rewrite - 4 August 2019 -------------------------------------------------------------------------------------------------------------------------------------------- 1.PerceptronAndGradient.py has been rewritten for computing Gradient Ascent and Descent for arbitrary number of variables. 2.Earlier loop for partial derivative delta computation for just 2 variables has been generalized for any number of variables 3.Gradient Ascent has also been added based on computed value of weight updated perceptron (+ instead of -) 4.Logs for Ascent and Descent are at testlogs/PerceptronAndGradient.log.4August2019 5.Hardcoded variable,weights,bias,rho,output values have been removed and made as arguments.

--- python-src/PerceptronAndGradient.py
+++ python-src/PerceptronAndGradient.py
@@ -1,5 +1,5 @@
-#------------------------------------------------------------------------------------------------------------
-#ASFER - a ruleminer which gets rules specific to a query and executes them (component of iCloud Platform)
+#-------------------------------------------------------------------------------------------------------
+#NEURONRAIN ASFER - Software for Mining Large Datasets
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
@@ -10,17 +10,12 @@
 #GNU General Public License for more details.
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#---------------------------------------------------------------------------------------------------------
-#Copyright (C):
-#Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
-#Ph: 9789346927, 9003082186, 9791165980
-#Krishna iResearch Open Source Products Profiles: 
-#http://sourceforge.net/users/ka_shrinivaasan, https://www.openhub.net/accounts/ka_shrinivaasan
+#--------------------------------------------------------------------------------------------------------
+#K.Srinivasan
+#NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
 #Personal website(research): https://sites.google.com/site/kuja27/
-#ZODIAC DATASOFT: https://github.com/shrinivaasanka/ZodiacDatasoft
-#emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com, kashrinivaasan@live.com
-#---------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------
+
 
 #Gradient Descent and Ascent for local minimum and maximum of a cost function
 #-------------------------------
@@ -35,44 +30,67 @@
 
 import rpy2.robjects as robj
 
-#For a linear non-sigmoid perceptron w1*x1+w2*x2+b in two variables for which weights have to be updated by Gradient
-#the L2 norm of the perceptron is differentiated wrt x1 and x2 and deltaw is computed
-def LinearPerceptronGradient():
-	outputs=[10.0,25.0,30.0,45.0,275.0]	
-	weights=[2.0,5.0]
+#For a linear non-sigmoid perceptron w1*x1+w2*x2+...+wn*xn+b  for which weights have to be updated by Gradient
+#the L2 norm of the perceptron is differentiated wrt x1,x2,...,xn and deltaw is computed
+def LinearPerceptronGradient(outputs,weights,rho,bias,variables):
 	converged=False
-	rho=-0.8
 	sum=0.0
-	bias=0.0
 	iteration=0
-	x1s=[1.0,2.0,5.0,7.0,8.0]
-	x2s=[5.0,3.0,6.0,7.0,2.0]
-	while not converged: 
-		deltaw1=0
-		deltaw2=0
-		for x1,x2,output in zip(x1s, x2s, outputs):
-			term = bias + weights[0]*x1 + weights[1]*x2 - output
-			term = term * x1
-			sum = sum + term	
-		deltaw1 = sum * 2	
-		for x1,x2,output in zip(x1s, x2s, outputs):
-			term = bias + weights[0]*x1 + weights[1]*x2 - output
-			term = term * x2
-			sum = sum + term	
-		deltaw2 = sum * 2	
-		weights[0] = weights[0] - rho * deltaw1	
-		weights[1] = weights[1] - rho * deltaw2	
-		print "LinearPerceptronGradient() weight update iteration: weights[0] = ",str(weights[0])
-		print "LinearPerceptronGradient() weight update iteration: weights[1] = ",str(weights[1])
-		print "LinearPerceptronGradient() weight update iteration: deltaw1 = ",str(deltaw1)
-		print "LinearPerceptronGradient() weight update iteration: deltaw2 = ",str(deltaw2)
+	while not converged and iteration < 1000: 
+                deltaw=[]
+                term=bias
+                varnum=0
+                sum=0.0
+                #Example iteration for two variables:
+                #-----------------------------------
+        	#for x1,x2,output in zip(x1s, x2s, outputs):
+        	#	term = bias + weights[0]*x1 + weights[1]*x2 - output
+        	#	term = term * x2
+        	#	sum = sum + term	
+		#deltaw2 = sum * 2	
+                #------------------------------------------------------------------------------
+                #Following iteration is generic for arbitrary number of variables
+                #generalizing previous example for 2 variables
+                #------------------------------------------------------------------------------
+                for cnt1 in xrange(len(variables[0])-1):
+        	   for cnt2 in xrange(len(variables)-1):
+        	       term += weights[cnt2]*variables[cnt2][cnt1]
+                   term = term - outputs[cnt1]
+        	   term = term * variables[varnum][cnt1]
+        	   sum = sum + term	
+                   varnum += 1
+        	   deltaw.append(sum * 2)
+                for cnt in xrange(len(variables[0])-1):
+                    if compute_perceptron(weights,variables,outputs) == 1:
+	                print "LinearPerceptronGradient() weight update iteration: Descending"
+		        weights[cnt] = weights[cnt] - rho * deltaw[cnt]
+                    else:
+                        print "LinearPerceptronGradient() weight update iteration: Ascending" 
+		        weights[cnt] = weights[cnt] + rho * deltaw[cnt]
+	            print "LinearPerceptronGradient() weight update iteration: weights[",cnt,"] = ",weights[cnt]
+		    print "LinearPerceptronGradient() weight update iteration: deltaw = ",deltaw
 		
-		if deltaw1*-1.0 < 0.0000001 and deltaw2*-1.0 < 0.0000001 or iteration==1000:
+                for cnt in xrange(len(weights)-1):
+		    if deltaw[cnt]*-1.0 < 0.0000001:
 			converged=True
-		print "input 1 to perceptron = " , x1s
-		print "input 2 to perceptron = " , x2s
+                    else:
+                        converged=False
 		print "weights updated after Gradient : " , weights
 		iteration+=1
-	
 
-LinearPerceptronGradient()
+def compute_perceptron(weights,variables,outputs):
+    for o in xrange(len(outputs)-1):
+        sum=0
+        for w in xrange(len(weights)-1):
+            sum = sum + weights[w]*variables[w][o]
+        if (outputs[o] - sum) > 10:
+            return 1
+    return 0
+	
+if __name__=="__main__":
+	outputs=[10.0,25.0,30.0,45.0,275.0]	
+	weights=[1.0,1.4,2.5,2.0,5.0]
+	rho=-0.8
+	bias=0.0
+	variables=[[1.0,2.0,5.0,6.0,8.0], [1.0,12.0,5.0,7.0,18.0], [1.0,22.0,35.0,7.0,8.0], [1.0,2.0,5.0,87.0,88.0], [1.0,12.0,5.0,7.0,8.0]]
+        LinearPerceptronGradient(outputs,weights,rho,bias,variables)
