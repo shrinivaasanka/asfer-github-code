d1e7a14a1e3449cccb2c61c31304f08ba491b9aa
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2019-08-12 13:33:04 +0530

    -------------------------------------------------------------------------------------------------------------------------------------------- 696. (FEATURE) Named Entity Recognition (NER)-HMM-Viterbi-CRF - Rewrite - 12 August 2019 -------------------------------------------------------------------------------------------------------------------------------------------- 1.Named Entity Recognition by Conditional Random Fields Hidden Markov Model has been updated significantly for resolving some errors in Viterbi computation 2.Prior Emission Probabilities computation by skew normal distribution has been updated to renormalize by dividing the skewnorm probability by sum of probabilities 3.New part of speech "adverb" has been included in transition probabilities,emission probabilities and states 4.weighted sknorm has been done away with and replaced by previous renormalized skewnorm distribution 5.Error in argmax() computation has been corrected - label and maximum are reset in the loop 6.logs for this rewrite have been committed to testlogs/NamedEntityRecognition_HMMViterbi_CRF.log.12August2019 7.Transition probabilities are updated as below: transition_probabilities={ 'noun':{'noun':0.0, 'verb':0.3, 'object':0.2, 'adjective':0.1, 'adverb':0.1, 'conjunction':0.3},                           'verb':{'noun':0.1, 'verb':0.0, 'object':0.4, 'adjective':0.2, 'adverb':0.1,'conjunction':0.3},                           'object':{'noun':0.0, 'verb':0.1, 'object':0.4, 'adjective':0.1, 'adverb':0.1,'conjunction':0.3},                           'adjective':{'noun':0.4, 'verb':0.2, 'object':0.2, 'adjective':0.0, 'adverb':0.1, 'conjunction':0.1},                           'adverb':{'noun':0.1, 'verb':0.4, 'object':0.1, 'adjective':0.0, 'adverb':0.1, 'conjunction':0.3},                           'conjunction':{'noun':0.2, 'verb':0.4, 'object':0.1, 'adjective':0.1, 'adverb':0.1, 'conjunction':0.1}                          } 7.It has to be mentioned here that previous state transition probabilities priors are hardcoded heuristics and based on the following rationale: 	7.1 verb follows nouns more often 	7.2 object follows verb more often 	7.3 object follows object more often 	7.4 noun follows adjective more often 	7.5 verb follows adverb more often 	7.6 verb follows conjunction more often (e.g infinitives) 8.Previous heuristics are minimal and could be replaced by more sophisticated probability distribution learnt from a training dataset once and loaded everytime.

--- python-src/NamedEntityRecognition_HMMViterbi_CRF.py
+++ python-src/NamedEntityRecognition_HMMViterbi_CRF.py
@@ -30,30 +30,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 fig, ax = plt.subplots(1,1)
 
-states=["noun","conjunction","verb","adjective","object"]
+states=["noun","conjunction","verb","adjective","adverb","object"]
 
-start_probabilities={'noun':0.3, 'verb':0.2, 'object':0.2, 'adjective':0.1, 'conjunction':0.2}
+start_probabilities={'noun':0.3, 'verb':0.2, 'object':0.2, 'adjective':0.1, 'adverb':0.1, 'conjunction':0.2}
 
 #State Transition Probabilities
-transition_probabilities={ 'noun':{'noun':0.0, 'verb':0.2, 'object':0.2, 'adjective':0.2, 'conjunction':0.4},
-			   'verb':{'noun':0.1, 'verb':0.0, 'object':0.3, 'adjective':0.4, 'conjunction':0.2},
-			   'object':{'noun':0.0, 'verb':0.2, 'object':0.0, 'adjective':0.1, 'conjunction':0.7},
-			   'adjective':{'noun':0.2, 'verb':0.2, 'object':0.4, 'adjective':0.0, 'conjunction':0.2},
-			   'conjunction':{'noun':0.2, 'verb':0.4, 'object':0.1, 'adjective':0.1, 'conjunction':0.2}
+transition_probabilities={ 'noun':{'noun':0.0, 'verb':0.3, 'object':0.2, 'adjective':0.1, 'adverb':0.1, 'conjunction':0.3},
+			   'verb':{'noun':0.1, 'verb':0.0, 'object':0.4, 'adjective':0.2, 'adverb':0.1,'conjunction':0.3},
+			   'object':{'noun':0.0, 'verb':0.1, 'object':0.4, 'adjective':0.1, 'adverb':0.1,'conjunction':0.3},
+			   'adjective':{'noun':0.4, 'verb':0.2, 'object':0.2, 'adjective':0.0, 'adverb':0.1, 'conjunction':0.1},
+			   'adverb':{'noun':0.1, 'verb':0.4, 'object':0.1, 'adjective':0.0, 'adverb':0.1, 'conjunction':0.3},
+			   'conjunction':{'noun':0.2, 'verb':0.4, 'object':0.1, 'adjective':0.1, 'adverb':0.1, 'conjunction':0.1}
 			 }
 
-#Observation Probabilities in a State
-#emission_probabilities={ 'noun':{'PoSTagging':0.7, 'is':0.1, 'used':0.0, 'to':0.1, 'annotate':0.1, 'complex':0.0, 'sentences':0.0, 'with':0.0,'PoS':0.0},
-#			 'verb':{'PoSTagging':0.1, 'is':0.3, 'used':0.2, 'to':0.0, 'annotate':0.3, 'complex':0.0, 'sentences':0.1, 'with':0.0,'PoS':0.0},
-#			 'object':{'PoSTagging':0.1, 'is':0.1, 'used':0.2, 'to':0.1, 'annotate':0.1, 'complex':0.0, 'sentences':0.4, 'with':0.0,'PoS':0.0},
-#			 'adjective':{'PoSTagging':0.1, 'is':0.1, 'used':0.1, 'to':0.0, 'annotate':0.0, 'complex':0.5, 'sentences':0.1, 'with':0.1,'PoS':0.0},
-#			 'conjunction':{'PoSTagging':0.0, 'is':0.1, 'used':0.0, 'to':0.5, 'annotate':0.0, 'complex':0.0, 'sentences':0.0, 'with':0.4,'PoS':0.0}
-#			} 
 
 #Part of Speech Tagging - Named Entity Recognition
-#observations=['PoSTagging', 'is', 'used', 'to', 'annotate', 'complex', 'sentences', 'with','PoS'] 
-#observations=['Python','is','interpreted']
-
 obs_file=open("NamedEntityRecognition_HMMViterbi_CRF.observations","r")
 observations=obs_file.read().split()
 
@@ -62,18 +53,13 @@ cnt=len(states)
 
 for s in states:
 	emissionsdict={}
-	x = np.linspace(skewnorm.ppf(0.01,5,cnt*0.5,1),skewnorm.ppf(0.99,5,cnt*0.5,1),len(observations))
-	sknormpdf = skewnorm.pdf(x,5,cnt*0.5,1)
-	sknormpdf_weighted=[]
-	for p,q in zip(sknormpdf,x):
-		sknormpdf_weighted.append(p*q*0.01)
-	print "skewnorm pdf weighted:",sknormpdf_weighted
-	ax.plot(x, skewnorm.pdf(x,5,cnt*0.5,1),'r-', lw=5, alpha=0.6, label='skewnorm pdf')
+	x = np.linspace(skewnorm.ppf(0.01,5,cnt*0.5,1),skewnorm.ppf(0.99,5,loc=cnt,scale=1),len(observations))
+	sknormpdf = skewnorm.pdf(x,5,loc=cnt,scale=1)
+	print "sknormpdf:",sknormpdf
+	ax.plot(x, sknormpdf,'r-', lw=5, alpha=0.6, label='skewnorm pdf')
 	obs_cnt=0
 	for o in observations:
-		#print "emissiondict[o] = ",float(obs_cnt+cnt)/float(len(states) + len(observations))
-		#sknorm=skewnorm(cnt)
-		emissionsdict[o]=sknormpdf_weighted[obs_cnt]
+		emissionsdict[o]=sknormpdf[obs_cnt]/sum(sknormpdf)
 		obs_cnt += 1
 	emission_probabilities[s]=emissionsdict
 	cnt=(cnt-1)
@@ -139,7 +125,7 @@ PoS_probabilities={}
 
 weight=0.01
 k=0.2
-maximum=-1
+maximum=0
 feature={}
 i=0
 missing_PoS=[]
@@ -157,15 +143,18 @@ print "PoS tagged words in previous sentence", PoS_CRF_states
 print "words missing PoS tag:",missing_PoS
 
 for t in zip(missing_PoS, prev_PoS_tag):
+	label=""
+	maximum=0
 	for s in states:
 		#Compute Feature function for missing PoS - definition of Feature function is subjective
 		#Here the feature function to find argmax() is the conditional probability product - discriminative model:
 		#	probability of transition from a previous labelled state * probability of observation in the state 
 		feature[s]=emission_probabilities[s][t[0]]*transition_probabilities[t[1]][s]
-	
 		PoS_probabilities[s]=k*math.exp(weight*feature[s])
+		print "argmax() computation for observations missing PoS tags: emission(s=",s,",t[0]=",t[0],")*transition(t[1]=",t[1],",s=",s,") - feature[s]=",feature[s],", PoS_probabilities[s] =",PoS_probabilities[s]
 		if PoS_probabilities[s] > maximum:
+		#if feature[s] > maximum:
 			maximum = PoS_probabilities[s]
+			#maximum = feature[s]
 			label=s
-
 	print " \"",t[0],"\" PoS tagged using CRF as: ",label
