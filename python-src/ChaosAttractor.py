69e876fc6073ef4423445f2a765661639dcb91c2
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2020-01-30 12:14:40 +0530

    --------------------------------------------------------------------------------------------------------------------------------------------- 744. (THEORY and FEATURE) Chaotic Pseudorandom Generator - Python 3.7.5 upgrade, Computational Geometric Factorization - 30 January 2020 - related to all sections on Pseudorandom Generators, Chaos and Factorization --------------------------------------------------------------------------------------------------------------------------------------------- 1.This commit upgrades Chaos Pseudorandom Generator implementation to Python 3.7.5 by autopep8 followed by 2to3, which is the defacto NeuronRain recommended python version for performance reasons. 2.Both Logistic and Lehmer-Palmore PRGs have been tested on Python 3.7.5 + R-py2 combine and logs are committed to testlogs/ChaosAttractor.Python3.7.5Upgrade.30January2020 3.GRAFIT course material on Geometric search of BigData in https://github.com/shrinivaasanka/Grafit/blob/master/course_material/NeuronRain/LinuxKernelAndCloud/BigdataAnalyticsCloud_CourseNotes.txt describe a Randomized NC factorization algorithm based on Parallel Chaotic Pseudorandom Generator and Central Limit Theorem based Space filling Linear Program mentioned in drafts - https://sites.google.com/site/kuja27/ChaoticPRG.pdf and https://sites.google.com/site/kuja27/Analysis%20of%20a%20Randomized%20Space%20Filling%20Algorithm%20and%20its%20Linear%20Program%20Formulation.pdf . 4.Exact PRAM-NC-BSP computational geometric factorization algorithm implemented on Spark in NeuronRain AstroInfer is the derandomized version of Randomized NC Bernoulli trials factorization algorithm in https://github.com/shrinivaasanka/Grafit/blob/master/course_material/NeuronRain/LinuxKernelAndCloud/BigdataAnalyticsCloud_CourseNotes.txt which has the success probability of finding atleast one prime factor pr(m) = (1 - sqrt(2N)/(N*sqrt(2) - sqrt(2) - sqrt(N)))^(m-1)*(sqrt(2N)/(N*sqrt(2) - sqrt(2) - sqrt(N)) after m trials for square embedded hyperbolic arc. Expected number of trials E(m) before atleast one factor is found = sum_x=1-to-N(x*pr(x)) 5.Thus NeuronRain Theory drafts define 3 different Nick's class Computational Geometric Integer factorization algorithms - Randomized NC, Quantum NC, Exact NC - which transition from a randomized, cohered quantum state to classical exact decohered state.

--- python-src/ChaosAttractor.py
+++ python-src/ChaosAttractor.py
@@ -1,69 +1,78 @@
-#------------------------------------------------------------------------------------------------------------
-#NEURONRAIN ASFER - Software for Mining Large Datasets
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#---------------------------------------------------------------------------------------------------------
-#K.Srinivasan
-#NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
-#Personal website(research): https://sites.google.com/site/kuja27/
-#---------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------
+# NEURONRAIN ASFER - Software for Mining Large Datasets
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------------------------------------------------------------------------------
+# K.Srinivasan
+# NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+# Personal website(research): https://sites.google.com/site/kuja27/
+# ---------------------------------------------------------------------------------------------------------
 
 import rpy2.robjects as robj
 
-#non-linearity starts from k >= 3 and below 3 there are periods
-input_seq=[]
-#correlation_flag="linear"
-correlation_flag="chaotic"
+# non-linearity starts from k >= 3 and below 3 there are periods
+input_seq = []
+# correlation_flag="linear"
+correlation_flag = "chaotic"
+
 
 def plotGraph(x):
-	no_of_hashes=x*100
-	for i in xrange(int(no_of_hashes)):
-		print "#",
-	print "\n"
+    no_of_hashes = x*100
+    for i in range(int(no_of_hashes)):
+        print("#", end=' ')
+    print("\n")
 
-#Chaotic PRG implementation - Verhulste's Logistic equation and Lehmer-Palmore Pseudorandom generator
-def ChaosPRG(algorithm="Logistic",seqlen=100,radix=10,initialcondition=0.7,prime=104729):
-    x=initialcondition
-    k=radix
-    chaos_seq=[]
-    print "ChaosPRG() algorithm:",algorithm
+# Chaotic PRG implementation - Verhulste's Logistic equation and Lehmer-Palmore Pseudorandom generator
+
+
+def ChaosPRG(algorithm="Logistic", seqlen=100, radix=10, initialcondition=0.7, prime=104729):
+    x = initialcondition
+    k = radix
+    chaos_seq = []
+    print("ChaosPRG() algorithm:", algorithm)
     while True:
         if algorithm == "Logistic":
-	    xnext = k*x*(1-x)	
+            xnext = k*x*(1-x)
         if algorithm == "Lehmer-Palmore":
             xnext = (k * x) % prime
-	print "x: ",x,", xnext: ",xnext
-	#plotGraph(xnext)
-	chaos_seq.append(float(x)*100000)
-	x=xnext
-	if len(chaos_seq) == seqlen:
-		break
+        print("x: ", x, ", xnext: ", xnext)
+        # plotGraph(xnext)
+        chaos_seq.append(float(x)*100000)
+        x = xnext
+        if len(chaos_seq) == seqlen:
+            break
     return chaos_seq
 
-if __name__=="__main__":
-    #Read input sequence
-    #f=open("ChaosAttractorInputSequence.txt")
-    f=open("ChaosAttractorInputSequence_DJIA.txt")
-    f_str=f.read()
-    input_seq=f_str.split(",")
 
-    #using rpy2.objects and compute R correlation coefficient
+if __name__ == "__main__":
+    # Read input sequence
+    # f=open("ChaosAttractorInputSequence.txt")
+    f = open("ChaosAttractorInputSequence_DJIA.txt")
+    f_str = f.read()
+    input_seq = f_str.split(",")
+
+    # using rpy2.objects and compute R correlation coefficient
     if correlation_flag == "chaotic":
-        chaosx = robj.FloatVector(ChaosPRG("Logistic",19449,3.8,0.7,104729))
-        #chaosx = robj.FloatVector(ChaosPRG("Lehmer-Palmore",19449,3.8,0.7,104729))
+        chaosx1 = robj.FloatVector(
+            ChaosPRG("Logistic", 19449, 3.8, 0.7, 104729))
+        chaosx2 = robj.FloatVector(ChaosPRG("Lehmer-Palmore",19449,3.8,0.7,104729))
     if correlation_flag == "linear":
-	chaosx = robj.FloatVector(xrange(19449))
-    print "Chaotic PRG sequence:",chaosx 
+        chaosx3 = robj.FloatVector(range(19449))
+    print("Chaotic PRG sequence - Logistic:", chaosx1)
+    print("Chaotic PRG sequence - Lehmer-Palmore:", chaosx2)
     inputstreamy = robj.FloatVector(input_seq[0:19449])
     corfn = robj.r['cor']
-    corfn(chaosx,inputstreamy)
-    ret=corfn(chaosx,inputstreamy)
-    print "correlation coefficient of pseudorandom and DJIA sequences is:", (ret.r_repr())
+    ret1 = corfn(chaosx1, inputstreamy)
+    ret2 = corfn(chaosx2, inputstreamy)
+    print("correlation coefficient of pseudorandom (Logistic) and DJIA sequences is:", (ret1.r_repr(
+    )))
+    print("correlation coefficient of pseudorandom (Lehmer-Palmore) and DJIA sequences is:", (ret2.r_repr(
+    )))
