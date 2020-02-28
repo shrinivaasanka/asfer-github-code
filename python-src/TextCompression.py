cd2e69664b3ef53898c85a5eaa50338902e89b08
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2020-02-28 14:23:01 +0530

    ---------------------------------------------------------------------------------------------------------------------------------------------- 750. (THEORY and FEATURE) Text Compression, Compressed Sensing, Syllable boundaries decompression implementation - Python 3.7.5 upgrade and refactoring - 28 February 2020, 29 February 2020 ---------------------------------------------------------------------------------------------------------------------------------------------- 1.TextCompression.py vowelless text compression-decompression implementation has been upgraded to Python 3.7.5 by autopep8 and 2to3 utilities 2.Syllable boundaries text compression function from CompressedSensing.py has been imported to optionally choose between syllable boundary compression of text and earlier vowelless compression (which strips vowels across the board) by a boolean flag syllable_boundary 3.Earlier commented code for PyEnchant spelling suggestions has been uncommented to choose between PyEnchant maximum likelihood (prob() function) and Hidden Markov Model maximum likelihood (HiddenMarkovModel_MLE() function) estimators based on if suggestions is not-None and None respectively. 4.Both PyEnchant and HMM computed maximum likelihood words are printed to logs. 5.Logs at testlogs/TextCompression.log.28February2020 contain PyEnchant and Hidden Markov Model maximum likelihood estimations per decompressed string for both vowel compressed and syllable boundary compressed words 6.decompressedtext.txt contains the newly introduced approximate syllable boundary decompressed text:
    ghts fer over  with lus tins    der ly2 on dead ster nal     ve tting  pro mer dial   placed be form byss paw er ful  ry mev ing  grant  ybody wn ters ician milk  shy ser pant ged form ing cotch shed  sh mania
    
    for exact text:
    Rama gets forever pleased with him who listens to or reads Ramayana daily. He is indeed the eternal Vishnu, the Lord of preservation. Rama is the primordial Lord, clearly placed before the eyes the powerful Lord removing the sins and the great-armed, who has abode on waters of the ocean of milk Sesha the serpent-god forming his couch is said to be Lakshmana.
    
    7. It is worth observing how the words "primordial", "removing","serpent" and "powerful" have been syllable decompressed - "pro mer dial", "ry mev ing", "ser pant", "paw er ful" - to homophones (phonetically equivalent but linguistically different)
    8.Advantage of syllable boundaries is their language and script independence, dependence on phonetics alone, fine-graining of words to syllables for better accuracy in phonetic decompression.
    9.Embedding a text on syllable vector space as opposed to traditional flat one-dimension thus blurs linguistic boundaries and useful for similar name clustering.
    
    References:
    -----------
    750.1 Phonetic Match Rating Algorithm - for similar name clustering in airlines - https://ia801704.us.archive.org/2/items/accessingindivid00moor/accessingindivid00moor.pdf

--- python-src/TextCompression.py
+++ python-src/TextCompression.py
@@ -17,6 +17,9 @@
 # --------------------------------------------------------------------------------------------------------
 
 import WordSubstringProbabilities
+import CompressedSensing
+
+syllable_boundary = True 
 
 
 class VowellessText:
@@ -28,15 +31,15 @@ class VowellessText:
         # 500000 substring-probabilites entries or more.
 
         # Prior probabilites for "tha", "the", "thi", "tho", "thu" prefixes
-        #Probabilities_prefixes={"tha":0.4, "the":0.3, "thi":0.1, "tho":0.1, "thu":0.1, "ca":0.3, "ce":0.1, "ci":0.1, "co":0.4, "cu":0.1}
+        # Probabilities_prefixes={"tha":0.4, "the":0.3, "thi":0.1, "tho":0.1, "thu":0.1, "ca":0.3, "ce":0.1, "ci":0.1, "co":0.4, "cu":0.1}
 
         # Prior probabilites for "at", "et", "it", "ot", "ut" suffixes
-        #Probabilities_suffixes={"at":0.3, "et":0.2, "it":0.1, "ot":0.2, "ut":0.2, "as":0.1, "es":0.2, "is":0.4, "os":0.2, "us":0.1}
+        # Probabilities_suffixes={"at":0.3, "et":0.2, "it":0.1, "ot":0.2, "ut":0.2, "as":0.1, "es":0.2, "is":0.4, "os":0.2, "us":0.1}
         prefixsuffixprob = WordSubstringProbabilities.wordprefixsuffixsubstringsprobdist()
         self.Probabilities_prefixes = prefixsuffixprob[0]
         self.Probabilities_suffixes = prefixsuffixprob[1]
         # self.likelydict=prefixsuffixprob[2]
-        #print "prefixsuffixprob=",prefixsuffixprob
+        # print "prefixsuffixprob=",prefixsuffixprob
 
         maxk1 = ""
         maxv1 = -1.0
@@ -47,7 +50,7 @@ class VowellessText:
             if maxv1 < v1:
                 maxv1 = v1
                 maxk1 = k1
-                #maxvowel = maxk1[len(maxk1)-1]
+                # maxvowel = maxk1[len(maxk1)-1]
 
         for k2, v2 in list(self.Probabilities_suffixes.items()):
             if maxv2 < v2:
@@ -82,38 +85,38 @@ class VowellessText:
     def compute_maximumlikelihood(self, comp, suggestions=None):
         maxlikely = ""
         maxlikelyprob = 0.0
-        # for x in suggestions:
-        # Bayesian
-        # probability=prob(x,comp)
-        # if probability > maxlikelyprob:
-        #	maxlikelyprob = probability
-        #	maxlikely=x
-        maxlikely = self.HiddenMarkovModel_MLE(comp)
-        print("comp=", comp, "; maxlikely=", maxlikely)
+        if suggestions is not None:
+            for x in suggestions:
+                probability = self.prob(x, comp)
+                if probability > maxlikelyprob:
+                    maxlikelyprob = probability
+                    maxlikely = x
+        else:
+            maxlikely = self.HiddenMarkovModel_MLE(comp)
+        print(("comp=", comp, "; maxlikely=", maxlikely))
         return maxlikely
 
-# def prob(x,comp):
-#	if len(x) > len(comp):
-#		xptr=0
-#		compptr=0
-#		print "x=",x,"; comp=",comp
-#		while xptr < len(x) and compptr < len(comp):
-#			while xptr < len(x):
-#				if x[xptr] in vowels and comp[compptr] != x[xptr]:
-#					xptr=xptr+1
-#			compptr=compptr+1
-#		if xptr == len(x) and compptr == len(comp):
-#			print "x = ",x,";comp = ",comp,"; return 1"
-#			return 1.0
-#		else:
-#			return 0.0
-#
+    #def prob(self, x, comp):
+    #    if len(x) > len(comp):
+    #        xptr = 0
+    #        compptr = 0
+    #        print("x=", x, "; comp=", comp)
+    #        while xptr < len(x) and compptr < len(comp):
+    #            while xptr < len(x):
+    #                if x[xptr] in vowels and comp[compptr] != x[xptr]:
+    #                    xptr = xptr+1
+    #            compptr = compptr+1
+    #        if xptr == len(x) and compptr == len(comp):
+    #            print("x = ", x, ";comp = ", comp, "; return 1")
+    #            return 1.0
+    #        else:
+    #            return 0.0
 
     def HiddenMarkovModel_MLE(self, comp):
         # For loop that matches a compressed word to likely candidate words by left-to-right substring comparison
         # between compressed word's substrings and the dictionary entries and populates a matching word dictionary
         compsubstrings = comp.split("_")
-        print("compsubstrings: ", compsubstrings)
+        print(("compsubstrings: ", compsubstrings))
 
         # start=0
         # i=0
@@ -139,8 +142,8 @@ class VowellessText:
         #	compsubstrindex = -2
         #	start=0
         #	i=0
-        #likelydictwords = WordSubstringProbabilities.wordlikelydict(compsubstrings)
-        #likelydictmatchwords = []
+        # likelydictwords = WordSubstringProbabilities.wordlikelydict(compsubstrings)
+        # likelydictmatchwords = []
         # self.matchinglikelydict={}
         # for compsubstring in compsubstrings:
         #	likelydictmatchwordsfragment = [x for x in likelydictwords if x.find(compsubstring) != -1]
@@ -152,7 +155,7 @@ class VowellessText:
 
         # Returns the word with maximum likelihood probability
         # Word returned depends on the HMM priors in the hashtable
-        print("matchinglikelydict=", self.matchinglikelydict)
+        print(("matchinglikelydict=", self.matchinglikelydict))
         maxlikelyretprob = -1.0
         maxlikelyret = ""
         for k7, v7 in list(self.matchinglikelydict.items()):
@@ -160,8 +163,9 @@ class VowellessText:
                 maxlikelyret = k7
                 maxlikelyretprob = v7
 
-        print("maxlikelyretprob=", maxlikelyretprob, "; maxlikelyret = ", maxlikelyret)
-        print(self.matchinglikelydict)
+        print(("maxlikelyretprob=", maxlikelyretprob,
+               "; maxlikelyret = ", maxlikelyret))
+        print((self.matchinglikelydict))
         return maxlikelyret
 
     def prob(self, x, comp):
@@ -174,23 +178,27 @@ class VowellessText:
         setcomp = set(comp)
         # if setcomp.issubset(setx):
         setdiff = setx.difference(setcomp)
-        setdiffconso = [y for y in setdiff if y not in vowels]
+        setdiffconso = [y for y in setdiff if y not in self.vowels]
+        print(setdiffconso)
 
         if len(setdiffconso) == 0 and len(x) > len(comp):
             xptr = 0
             compptr = 0
-            print("x=", x, "; comp=", comp)
+            print(("x=", x, "; comp=", comp))
             while xptr < len(x) and compptr < len(comp):
                 while xptr < len(x) and comp[compptr] != x[xptr]:
-                    print("x[xptr]=", x[xptr], "; comp[compptr]=", comp[compptr])
+                    print(("x[xptr]=", x[xptr],
+                           "; comp[compptr]=", comp[compptr]))
                     xptr = xptr+1
                 compptr = compptr+1
             # Both pointers should have reached the end of the respective words
             if xptr == len(x) and compptr == len(comp):
-                print("x = ", x, ";comp = ", comp, "; return 1")
+                print(("x = ", x, ";comp = ", comp, "; return 1"))
                 return 1.0
             else:
                 return 0.0
+        else:
+            return 1.0
 
 
 if __name__ == "__main__":
@@ -201,6 +209,7 @@ if __name__ == "__main__":
 
     d = enchant.Dict("en_US")
     vowellesstxt = VowellessText()
+    csens = CompressedSensing.CompressedSensing()
     origfilesize = 0
     compfilesize = 0
     compbits = 0
@@ -209,32 +218,40 @@ if __name__ == "__main__":
     # Compression
     print("Compression by vowel removal:")
     for i in f:
-        wcomp = vowellesstxt.removevowels(i)
+        if syllable_boundary == False:
+            wcomp = vowellesstxt.removevowels(i)
+        else:
+            wcompcsens = csens.syllable_boundary_text_compression(i)
+            wcomp = " ".join(wcompcsens[2].split("-"))
+        print(("wcomp:", wcomp))
         # remove _ in compressed word
         for k in wcomp:
             if k != '_':
                 wcompcomp = wcompcomp + k
             else:
                 compbits = compbits+1
-        print("wcompcomp = ", wcompcomp)
+        print(("wcompcomp = ", wcompcomp))
         origfilesize = origfilesize + len(i)
         compfilesize = compfilesize + len(wcompcomp)
         for n in wcomp:
             fcomp.write(n)
-        print("Compression ratio = ", 100 * \
-            (origfilesize-compfilesize-0.125*compbits)/origfilesize, "%")
+        print(("Compression ratio = ", 100 *
+               (origfilesize-compfilesize-0.125*compbits)/origfilesize, "%"))
     fcomp.close()
 
     fcomp = open("compressedtext.txt", "r")
     fcompstr = fcomp.read()
 
     # Decompression
-    print("Decompression using PyEnchant Spellcheck suggest() function:")
+    print("Decompression using Hidden Markov Model or PyEnchant Spellcheck suggest() function:")
     for i in fcompstr.split():
         suggestions = d.suggest(i)
-        print(i, suggestions)
-        # midpoint=len(suggestions)
-        # maximumlikelihoodword=vowellesstxt.compute_maximumlikelihood(i,suggestions)
-        maximumlikelihoodword = vowellesstxt.compute_maximumlikelihood(i)
-        fdecomp.write(maximumlikelihoodword)
+        print((i, suggestions))
+        midpoint = len(suggestions)
+        maximumlikelihoodword_enchant = vowellesstxt.compute_maximumlikelihood(
+            i, suggestions)
+        maximumlikelihoodword_HMM = vowellesstxt.compute_maximumlikelihood(i)
+        print("maximumlikelihoodword - PyEnchant :",maximumlikelihoodword_enchant)
+        print("maximumlikelihoodword - HMM :",maximumlikelihoodword_HMM)
+        fdecomp.write(maximumlikelihoodword_HMM)
         fdecomp.write(" ")
