dc32e5d20958ea946fd86877585cd2e83a4fd32b
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2020-09-05 00:29:34 +0530

    --------------------------------------------------------------------------------------------------------------------------------------------- 865. (THEORY and FEATURE) NeuronRainApps - NeuronRain usecases - Drone Autonomous Online Shopping Delivery Implementation - MAVSDK + JMAVSIM simulation for PX4 - 1 September 2020 - related to 637,675,713 --------------------------------------------------------------------------------------------------------------------------------------------- 1. OnlineShoppingDelivery Python 3.7.5 implementation in NeuronRainApps/Drones has been changed to import MAVSDK Mission Plan classes and dronecode_sdk imports have been commented (Example: https://github.com/mavlink/MAVSDK-Python/blob/master/examples/mission.py). 2. Following GIS analytics variables are read by socket streaming datasource in Streaming Abstract Generator from a webserver listening on port 64001 (netcat -l 64001) and are parsed as key-value pairs (these variables have also been committed to python-src/NeuronRainApps/Drones/OnlineShoppingDelivery.GISvars.txt) delimited by end marker:         longitude=47.397825620791895         latitude=8.5450092830163281         relative_altitude_m=1         speed_m_s=1         is_fly_through = True         gimbal_pitch_deg = nan         gimbal_yaw_deg = nan         camera_action = none         loiter_time_s = nan         camera_photo_interval_s = nan         end 3. In this commit only first four variables for navigation are being read from socket stream while rest are assumed to have default values. 4. Code changes for MissionPlan specific to MAVSDK mission-upload, start-mission and arm-takeoff-take_photo have been committed. To simulate a hamiltonian cycle, drone.mission.set_return_to_launch_after_mission(True) has been invoked. 5. awaits for mission progress and observe_is_in_air have been commented. 6. GIS navigation analytics variables read over socket stream are filtered for convex-hull boundaries and mission items are appended accordingly to Mission plan. Every mission item is a delivery point. 7. JMAVSIM and PX4 SITL Logs, Netcat logs for streaming analytics variables and MP4 video of the simulated flight of a drone have been committed as hereunder:          python-src/NeuronRainApps/Drones/testlogs/OnlineShoppingDelivery_Drone_JMAVSIM_Flight_Simulation.log1.1September2020          python-src/NeuronRainApps/Drones/testlogs/OnlineShoppingDelivery_Drone_JMAVSIM_Flight_Simulation.log2.1September2020          python-src/NeuronRainApps/Drones/testlogs/OnlineShoppingDelivery_PX4_Drone_JMAVSIM_Flight_Simulation2.mp4 8. Video python-src/NeuronRainApps/Drones/testlogs/OnlineShoppingDelivery_PX4_Drone_JMAVSIM_Flight_Simulation2.mp4 shows the imaginary multirotor drone in JMAVSIM PX4 SITL making a vertical takeoff, drawing an aerial cycle based on longitude-latitude-altitude of mission item delivery points before comeback to origin by vertical landing.

--- python-src/NeuronRainApps/Drones/OnlineShoppingDelivery.py
+++ python-src/NeuronRainApps/Drones/OnlineShoppingDelivery.py
@@ -1,45 +1,57 @@
-#-------------------------------------------------------------------------------------------------------
-#NEURONRAIN ASFER - Software for Mining Large Datasets
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#--------------------------------------------------------------------------------------------------------
-#K.Srinivasan
-#NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
-#Personal website(research): https://sites.google.com/site/kuja27/
-#--------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------
+# NEURONRAIN ASFER - Software for Mining Large Datasets
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------------------------------
+# K.Srinivasan
+# NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+# Personal website(research): https://sites.google.com/site/kuja27/
+# --------------------------------------------------------------------------------------------------------
 
 
-#Analytics enabled Drone Mission - Online Shopping Delivery
-#----------------------------------------------------------
-#Following static mission plan example from DronecodeSDK has been changed to illustrate dynamic mission 
-#plan based on analytics:
-#Example Drone Mission - https://github.com/Dronecode/DronecodeSDK-Python/blob/master/examples/mission.py
-#Drone MissionItem Proto - https://github.com/Dronecode/DronecodeSDK-Proto/blob/a54b353d73ff8d6e36c716b5278990e0f8cb770c/protos/mission/mission.proto
+# Analytics enabled Drone Mission - Online Shopping Delivery
+# ----------------------------------------------------------
+# Following static mission plan example from DronecodeSDK has been changed to illustrate dynamic mission
+# plan based on analytics:
+# Example Drone Mission - https://github.com/Dronecode/DronecodeSDK-Python/blob/master/examples/mission.py
+# Drone MissionItem Proto - https://github.com/Dronecode/DronecodeSDK-Proto/blob/a54b353d73ff8d6e36c716b5278990e0f8cb770c/protos/mission/mission.proto
 
-#Disclaimer: This is not compileable, executable code but only a pseudocode and has not been tested on a drone because of lack of it and aviation licensing requirements
+# Disclaimer: This is not compileable, executable code but only a pseudocode and has not been tested on a drone because of lack of it and aviation licensing requirements
 
 #!/usr/bin/env python3
 
 import asyncio
 
-from dronecode_sdk import connect as dronecode_sdk_connect
-from dronecode_sdk import (MissionItem)
+#from dronecode_sdk import connect as dronecode_sdk_connect
+from mavsdk import System
+#from dronecode_sdk import (MissionItem)
+from mavsdk.mission import MissionItem,MissionPlan
 from ImageGraph_Keras_Theano import convex_hull
+import Streaming_AbstractGenerator
 
 
-drone = dronecode_sdk_connect(host="127.0.0.1")
-
+#drone = dronecode_sdk_connect(host="127.0.0.1")
+drone = System()
 
 async def run():
-    inputf=Streaming_AbstractGenerator.StreamAbsGen("Socket_Streaming","localhost")
+    await drone.connect()
+    inputf = Streaming_AbstractGenerator.StreamAbsGen(
+        "Socket_Streaming", "localhost")
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Drone discovered with UUID: {state.uuid}")
+            break
+
+    
     mission_items = []
     mission_items.append(MissionItem(47.398039859999997,
                                      8.5455725400000002,
@@ -72,74 +84,115 @@ async def run():
                                      float('nan'),
                                      float('nan')))
 
-    #Dynamic Mission Plan which autopilots the drone based on GIS analytics navigation variables 
+    # Dynamic Mission Plan which autopilots the drone based on GIS analytics navigation variables
     # - e.g. longitude, latitude, altitude, speed, camera action etc., -
-    #read by Streaming Abstract Generator socket streaming and appends MissionItems to
-    #flight plan dynamically restricted by ordinates convex hull. When drone is within 
-    #convex hull airspace, its altitude is set to 0 (or minimal value) and landed
-    convex_hull_longlats=tolonglat(convex_hull("testlogs/SEDAC_GIS_ChennaiMetropolitanArea.jpg"))
-    for gis_analytics in inputf:
-    	longitude=gis_analytics[0]
-    	latitude=gis_analytics[1]
-    	relative_altitude_m=gis_analytics[2]
-    	speed_m_s=gis_analytics[3]
-    	is_fly_through=gis_analytics[4]
-    	gimbal_pitch_deg=gis_analytics[5]
-    	gimbal_yaw_deg=gis_analytics[6]
-    	camera_action=gis_analytics[7]
-	if camera_action==TAKE_PHOTO:
-		try:
-			drone.camera.take_photo()
-		except CameraError:
-			print "Camera Error"
-    	loiter_time_s=gis_analytics[8]
-    	camera_photo_interval_s=gis_analytics[9]
-    
-	if (longitude,latitude) not in convex_hull_longlats:
-    		mission_items.append(MissionItem(longitude,
-		latitude,
-		relative_altitude_m,
-		speed_m_s,
-		is_fly_through,
-		gimbal_pitch_deg,
-		gimbal_yaw_deg,
-		camera_action,
-		loiter_time_s,
-    		camera_photo_interval_s))
-	else:
-		mission_items.append(MissionItem(longitude,
-		latitude,
-		0,
-		0,
-		is_fly_through,
-		gimbal_pitch_deg,
-		gimbal_yaw_deg,
-		camera_action,
-		loiter_time_s,
-    		camera_photo_interval_s))
-
-
-    await drone.mission.set_return_to_launch_after_mission(True)
+    # read by Streaming Abstract Generator socket streaming and appends MissionItems to
+    # flight plan dynamically restricted by ordinates convex hull. When drone is within
+    # convex hull airspace, its altitude is set to 0 (or minimal value) and landed
+    convex_hull_longlats = tolonglat(convex_hull(
+        "../../image_pattern_mining/ImageNet/testlogs/SEDAC_GIS_ChennaiMetropolitanArea.jpg"))
+    variables_read=False
+    longitude = latitude = relative_altitude_m =  speed_m_s = is_fly_through =  gimbal_pitch_deg = gimbal_yaw_deg = camera_action = loiter_time_s = camera_photo_interval_s = 0 
+    for gis_analytics_kv_binary in inputf:
+        gis_analytics_kv_lines=str(gis_analytics_kv_binary).split("\\n")
+        for gis_analytics_kv in gis_analytics_kv_lines:
+            gis_analytics_kv_tok=str(gis_analytics_kv).split("=")
+            if 'longitude' in str(gis_analytics_kv_tok[0]).strip():
+                longitude = float(str(gis_analytics_kv_tok[1]).strip())
+                print(longitude)
+            if 'latitude' in str(gis_analytics_kv_tok[0]).strip():
+                latitude = float(str(gis_analytics_kv_tok[1]).strip())
+                print(latitude)
+            if 'relative_altitude_m' in str(gis_analytics_kv_tok[0]).strip():
+                relative_altitude_m = int(str(gis_analytics_kv_tok[1]).strip()) + 25
+                print(relative_altitude_m)
+            if 'speed_m_s' in str(gis_analytics_kv_tok[0]).strip():
+                speed_m_s = int(str(gis_analytics_kv_tok[1]).strip()) + 10
+                print(speed_m_s)
+            #if gis_analytics_kv_tok[0] == "is_fly_through":
+            #    is_fly_through = gis_analytics_kv_tok[1]
+            #    if is_fly_through == "True":
+            #        is_fly_through = True
+            #    else:
+            #        is_fly_through = False
+            #if gis_analytics_kv_tok[0] == "gimbal_pitch_deg":
+            #    gimbal_pitch_deg = float(gis_analytics_kv_tok[1])
+            #if gis_analytics_kv_tok[0] == "gimbal_yaw_deg":
+            #    gimbal_yaw_deg = float(gis_analytics_kv_tok[1])
+            #if gis_analytics_kv_tok[0] == "camera_action":
+            #    if gis_analytics_kv_tok[1] == "1":
+            #        camera_action =  MissionItem.CameraAction.TAKE_PHOTO
+            #    else:
+            #        camera_action =  MissionItem.CameraAction.NONE
+            #if gis_analytics_kv_tok[0] == "loiter_time_s":
+            #    loiter_time_s = float(gis_analytics_kv_tok[1])
+            #if gis_analytics_kv_tok[0] == "camera_photo_interval_s":
+            #    camera_photo_interval_s = float(gis_analytics_kv_tok[1])
+            if str(gis_analytics_kv)=="end":
+                variables_read=True 
+                break
+        if variables_read:
+            break
+    if (longitude, latitude) not in convex_hull_longlats:
+            print("Outside convex hull: GIS Analytics Variables Read...Mission Items being appended...")
+            mission_items.append(MissionItem(longitude,
+                                             latitude,
+                                             relative_altitude_m,
+                                             speed_m_s,
+                                             True,
+                                             float('nan'),
+                                             float('nan'),
+                                             MissionItem.CameraAction.NONE,
+                                             float('nan'),
+                                             float('nan')))
+    else:
+            print("Inside convex hull: GIS Analytics Variables Read...Mission Items being appended...")
+            mission_items.append(MissionItem(longitude,
+                                             latitude,
+                                             0.0,
+                                             0.0,
+                                             True,
+                                             float('nan'),
+                                             float('nan'),
+                                             MissionItem.CameraAction.NONE,
+                                             float('nan'),
+                                             float('nan')))
 
     print("-- Uploading mission")
-    await drone.mission.upload_mission(mission_items)
-
+    mission_plan=MissionPlan(mission_items)
+    await drone.mission.set_return_to_launch_after_mission(True)
+    await drone.mission.upload_mission(mission_plan)
+    
     print("-- Arming")
     await drone.action.arm()
+
+    print("-- Vertical Takeoff")
+    await drone.action.takeoff()
 
     print("-- Starting mission")
     await drone.mission.start_mission()
 
+    print("-- Taking photo")
+    if camera_action == MissionItem.CameraAction.TAKE_PHOTO:
+        try:
+            drone.camera.take_photo()
+        except CameraError:
+            print("Camera Error")
+    #await asyncio.ensure_future(print_mission_progress())
+    #await asyncio.ensure_future(observe_is_in_air())
 
 async def print_mission_progress():
+    await drone.connect()
     async for mission_progress in drone.mission.mission_progress():
-        print(f"Mission progress: {mission_progress.current_item_index}/{mission_progress.mission_count}")
+        #print(f"Mission progress: {mission_progress.current_item_index}/{mission_progress.mission_count}")
+        print(f"Mission progress: {mission_progress.current}/{mission_progress.total}")
 
 
 async def observe_is_in_air():
     """ Monitors whether the drone is flying or not and
     returns after landing """
 
+    await drone.connect()
     was_in_air = False
 
     async for is_in_air in drone.telemetry.in_air():
@@ -150,13 +203,14 @@ async def observe_is_in_air():
             await asyncio.get_event_loop().shutdown_asyncgens()
             return
 
+
 def tolonglat(convexhull):
-    #hardcoded
-    return [(11,11),(12,12),(78,07)]
+    # hardcoded
+    return [(11, 11),(12, 12),(78, 7)]
 
 def setup_tasks():
     asyncio.ensure_future(run())
-    asyncio.ensure_future(print_mission_progress())
+    #asyncio.ensure_future(print_mission_progress())
 
 
 if __name__ == "__main__":
