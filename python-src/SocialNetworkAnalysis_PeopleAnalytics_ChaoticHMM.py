0b2386b5e4e48ff7c37b8f7e0c75ae7d01dce0a4
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2020-12-28 12:26:05 +0530

    --------------------------------------------------------------------------------------------------------------------------------------------- 1130. (THEORY and FEATURE) People Analytics - HR and Talent Analytics - Chaotic Hidden Markov Model - Career Polynomial of a Profile and Inner Product Space Distance Similarity between 2 people profiles - related to 753,1127,1128,1129 and all sections on People Analytics, Intrinsic Merit Versus Fame Equilibrium, BKS Conjecture, Stability of Interview LTF/PTF - 30 December 2020 --------------------------------------------------------------------------------------------------------------------------------------------- 1.This commit implements 3 new functions in python-src/SocialNetworkAnalysis_PeopleAnalytics_ChaoticHMM.py - career_polynomial_inner_product_distance(),normalize(),second() - and polynomial fit of weighted HMM observations (designations) in career_transition_analytics() 2.career_polynomial_inner_product_distance() takes two arrays of points on a polynomial (of same size sampled on similar time points which could be numeric values for designations,remunerations or durations), polyfit()-s the points to two polynomials and computes inner product of the two polynomials which is the clustering similarity measure for two people profiles. 3.second() is a  utility function for getting the second element in an array of ordered tuples. Usual cast of array of ordered tuple to dict() and invoking values() on them returns unique values and not a multiset of repetitive values which is redressed by second(). 4.Utility Function normalize() divides a numeric array by maximum element which might be necessary for comparing trends in remunerations,designations and durations of two talent profiles. 5.Apart from these, weighted observations (designations) within Chaotic HMM are as well fitted to a polynomial in career_transition_analytics() 6.All polynomials are of degree 5 and logs testlogs/SocialNetworkAnalysis_PeopleAnalytics_ChaoticHMM.log.30December2020 demonstrate the inner product distance measures and fitted career polynomials through few example inputs.                                                                                                                             16553,1       Bot

--- python-src/SocialNetworkAnalysis_PeopleAnalytics_ChaoticHMM.py
+++ python-src/SocialNetworkAnalysis_PeopleAnalytics_ChaoticHMM.py
@@ -34,6 +34,7 @@ from collections import Counter
 import json
 import tensorflow as tf
 import tensorflow_io as tfio
+from numpy import polyfit
 
 fig, ax = plt.subplots(1, 1)
 
@@ -94,6 +95,36 @@ class ChaoticHMM(object):
         if prev_obs in self.designations["business"].keys():
             self.career_transition_score += self.designations["business"][prev_obs]
         print("career_transition_analytics(): career transition score of the profile = ",self.career_transition_score)
+        self.weighted_designations=dict(list(self.designations["academic"].items()) + list(self.designations["work"].items()) + list(self.designations["startup"].items()) + list(self.designations["business"].items()))
+        career_polynomial_points=[]
+        for o in self.observations:
+            career_polynomial_points.append((o,self.weighted_designations[o]))
+        print("career_transition_analytics(): career_polynomial_points:",career_polynomial_points)
+        career_polynomial=polyfit(list(range(len(career_polynomial_points))),self.second(career_polynomial_points),5)
+        print("career_transition_analytics(): career polynomial - based on weighted designations (HMM observations):",career_polynomial)
+
+    def career_polynomial_inner_product_distance(self,poly1,poly2):
+        career_polynomial1=polyfit(range(len(poly1)),poly1,5)
+        career_polynomial2=polyfit(range(len(poly2)),poly2,5)
+        ip_distance=0.0
+        for x,y in zip(poly1,poly2):
+            ip_distance += x*y
+        print("career_polynomial_inner_product_distance(): career_polynomial1 = ",career_polynomial1)
+        print("career_polynomial_inner_product_distance(): career_polynomial2 = ",career_polynomial2)
+        print("career_polynomial_inner_product_distance(): ip_distance = ",ip_distance)
+        return ip_distance
+
+    def second(self,orderedpair):
+        values=[]
+        for op in orderedpair:
+            values.append(op[1])
+        return values
+
+    def normalize(self,iterable):
+        newx=[]
+        for x in iterable:
+            newx.append(x/max(iterable))
+        return newx
 
     def chaotic_HMM_viterbi(self):
         Viterbi = [{}]
@@ -187,3 +218,21 @@ if __name__ == "__main__":
                             chaotichmmjson["transition_probabilities"], chaotichmmjson["emission_probabilities"], chaotichmmjson["observations"])
     chaotichmm.chaotic_HMM_viterbi()
     chaotichmm.career_transition_analytics()
+    designations1=[1,2,3,4,5,6,7]
+    remunerations1=[100000,700000,1000000,1300000,200000,1400000,2500000]
+    durations1=[0.7,5,0.1,2,3,2,0.5]
+    designations2=[2,4,5,7,3,6]
+    remunerations2=[10000,70000,1000000,1300,20000,1500000,3500000]
+    durations2=[7,5,0.3,2,4,1,0.3]
+    print("========================================================")
+    print("Career Polynomial Inner Product Distance - Remunerations")
+    print("========================================================")
+    chaotichmm.career_polynomial_inner_product_distance(chaotichmm.normalize(remunerations1),chaotichmm.normalize(remunerations2))
+    print("========================================================")
+    print("Career Polynomial Inner Product Distance - Designations")
+    print("========================================================")
+    chaotichmm.career_polynomial_inner_product_distance(chaotichmm.normalize(designations1),chaotichmm.normalize(designations2))
+    print("========================================================")
+    print("Career Polynomial Inner Product Distance - Durations")
+    print("========================================================")
+    chaotichmm.career_polynomial_inner_product_distance(chaotichmm.normalize(durations1),chaotichmm.normalize(durations2))
