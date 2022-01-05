706d22f8560158152be8caf8b9887538e974e14c
Krishna iResearch - NeuronRain - K.Srinivasan <ka.shrinivaasan@gmail.com>
2022-01-05 22:35:35 +0530

    --------------------------------------------------------------------------------------------------------------------------------------------- 1194. (THEORY and FEATURE) GIS Analytics - DBSCAN Density Clustering implementation - related to 1182 and all sections on Merit of Large Scale Visuals, Text analytics, Unsupervised clustering, GIS and Urban Sprawl Analytics - Urban Extent Learners, Computational Geometry, Intrinsic Merit Rankings, R Ricker and SEDAC GPW4 Convex Hull Population Estimations, Community detection, Social Network Analytics, Space filling by Curves, Unequal Circle and Polynomial packing, Computational Geometry and Polygonal packing by Voronoi Tessellation, Four color theorem, Economic Merit, Computational Economics - 5 January 2022 --------------------------------------------------------------------------------------------------------------------------------------------- 1194.1 New source file DBSCANClustering.py has been added to repository which implements award winning DBSCAN clustering algorithm especially useful for visual clustering of images, GIS Mapping and Urban Sprawl Extent Learners (NASA SEDAC,VIIRS and recent state-of-the-art Landsat9 TIRS2 and OLI2 - https://www.nasa.gov/content/landsat-9-image-gallery). 1194.2 DBSCAN is intuitive and finds clusters by density concentration of points around a core point which is the cluster centroid. 1194.3 Following is a gist of DBSCAN algorithm pseudocode implemented in DBSCANClustering.py:         1194.3.1 For each core point find the neighbours of it bounded by minPoints by some distance function         1194.3.2 If number of neighbours is < minPoints mark the point as noise else create a new cluster label for the core point         1194.3.3 Initialize a seed set to neighbours of the core point and for all neighbour points of a core point find the neighbours (neighbours of neighbours - expands the cluster recursively till seedset is exhausted) and add them to the cluster by labelling - Graph theoretically, community of neighbours or a strongly connected component of neighbourhood is detected. 1194.4 DBSCANClustering.py finds neighbours (greater than minpoints) within a 4*(epsilon)^2 pixel area square bounding box (of side 2*epsilon) around a point after filtering below a threshold pixel value. Clusters are stored in a dictionary map of pixel-to-label. Present implementation of DBSCAN is visual clustering specific but could be used for any multidimensional text dataset after minor datatype changes to find_neighbours() and clustering() functions. DBSCAN clusters could be optionally invoked in place of OpenCV2 Contouring, Watershed Segmentation and Minimum enclosing circle boundaries of urban sprawl extents in image_pattern_mining/ImageNet/GISUrbanSprawlAnalytics.py. 1194.5 An example scatterplot visual image_pattern_mining/ImageNet/testlogs/GHSL_GIS_ChennaiMetropolitanArea.jpg of Chennai Metropolitan Sprawl Population Density from Global Human Settlement Layer (GHSL - Urban Centre Database 2018 - https://ghsl.jrc.ec.europa.eu/ucdb2018visual.php# ) has been DBSCAN clustered and written to image_pattern_mining/ImageNet/testlogs/GHSL_GIS_ChennaiMetropolitanArea_dbscanclustered.jpg by write_clustered_image() - clusters are color coded by DBSCAN cluster label. Only the less dense areas of the map are clustered perhaps due to the thresholding. 1194.6 Class DBSCAN is parametrized by minimum number of neighbourhood points, epsilon size of the rectangular neighbourhood and threshold for choosing pixels. 1194.7 Logs are written to testlogs/DBSCANClustering.log.5January2022 which include number of clusters found and cluster labelled pixel dictionary.

---
+++ python-src/DBSCANClustering.py
@@ -0,0 +1,107 @@
+# -------------------------------------------------------------------------------------------------------
+# ASFER - Software for Mining Large Datasets
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------------------------------
+# K.Srinivasan
+# NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+# Personal website(research): https://acadpdrafts.readthedocs.io/en/latest/
+# --------------------------------------------------------------------------------------------------------
+
+import math
+from PIL import Image
+import numpy as np
+import pprint
+import random
+import ImageToBitMatrix
+import cv2
+from scipy.spatial.distance import directed_hausdorff
+import matplotlib.pylab as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from scipy.interpolate import splprep, splev
+import networkx as nx
+from networkx.drawing.nx_pydot import write_dot
+from shapely.geometry import Polygon
+from scipy.stats import wasserstein_distance
+import dlib
+import netrd
+from networkx.algorithms import isomorphism
+from collections import defaultdict
+
+class DBSCAN(object):
+    def __init__(self,imagefile,threshold=200,epsilon=3,minpoints=10):
+        self.imagefile=imagefile
+        self.img = cv2.imread(imagefile,0)
+        #cv2.threshold(self.img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        print(self.img)
+        self.threshold = threshold
+        self.epsilon = epsilon
+        self.minpoints = minpoints
+        self.dbscandict = defaultdict(int)
+        self.noise=-1
+        self.undefined=0
+        self.clusterlabel=1
+
+    def find_neighbours(self,img,row,col,threshold,epsilon,minpoints):
+        neighbours=[]
+        for r in range(row-epsilon,row+epsilon):
+            for c in range(col-epsilon,col+epsilon):
+                if r > 0 and c > 0 and r < len(img)-1 and c < len(img[0])-1:
+                     if img[r][c] < threshold:
+                         neighbours.append((r, c))
+        #print("neighbours of (",row,",",col,")",neighbours)
+        return neighbours
+
+    def clustering(self):
+        for row in range(len(self.img)-1):
+            for col in range(len(self.img)-1):
+                if self.dbscandict[(row,col)]!=self.undefined:
+                    continue
+                neighbours = self.find_neighbours(self.img,row,col,self.threshold,self.epsilon,self.minpoints)
+                if len(neighbours) < self.minpoints:
+                    self.dbscandict[(row,col)]=self.noise
+                    continue
+                self.clusterlabel += 1
+                self.dbscandict[(row,col)]=self.clusterlabel
+                seedpoints=neighbours
+                if (row,col) in seedpoints:
+                    seedpoints.remove((row,col))
+                for s in seedpoints:
+                    if self.dbscandict[s]==self.noise:
+                        self.dbscandict[s]=self.clusterlabel
+                    if self.dbscandict[s]!=self.undefined:
+                        continue
+                    self.dbscandict[s]=self.clusterlabel
+                    s_neighbours=self.find_neighbours(self.img,s[0],s[1],self.threshold,self.epsilon,self.minpoints)
+                    if len(s_neighbours) >= self.minpoints:
+                        seedpoints = seedpoints + s_neighbours
+                    #seedpoints.remove(s)
+        print("DBSCAN cluster labelling of pixels:",self.dbscandict)
+
+    def write_clustered_image(self):
+        number_of_clusters=len(set(self.dbscandict.values()))
+        print("number of DBSCAN density clusters:",number_of_clusters)
+        print("size of image:",self.img.shape[0] * self.img.shape[1])
+        print("size of dbscandict:",len(self.dbscandict.items()))
+        dbscanimg = cv2.imread(self.imagefile)
+        for k,v in self.dbscandict.items():
+            cluster=self.dbscandict[k]
+            dbscanimg[k[0],k[1]]=(cluster*2,cluster*3,cluster)
+        imagefiletoks = self.imagefile.split(".")
+        cv2.imshow(imagefiletoks[0]+"_dbscanclustered.jpg",dbscanimg)
+        cv2.imwrite(imagefiletoks[0]+"_dbscanclustered.jpg",dbscanimg)
+        cv2.waitKey()
+
+if __name__=="__main__":
+    dbscan=DBSCAN("image_pattern_mining/ImageNet/testlogs/GHSL_GIS_ChennaiMetropolitanArea.jpg")
+    dbscan.clustering()
+    dbscan.write_clustered_image()
+
