62c5b8b06b8ef4206d1b895cf7fa4b75072c46dc
Krishna iResearch - NeuronRain - K.Srinivasan <ka.shrinivaasan@gmail.com>
2022-01-30 06:13:34 +0530

    --------------------------------------------------------------------------------------------------------------------------------------------- 1205. (THEORY and FEATURE) Computational Geometric Factorization - Matplotlib plot of benchmarks - 300 integers from 100 to 400 and 20 integers from 500 to 520 - related to 1159 and all sections on Computational Geometric Factorization, Neural network learning of Factors by SAC=TC=NC=AC equivalence, Planar Point Location - 30 January 2022 --------------------------------------------------------------------------------------------------------------------------------------------- 1205.1 New source file python-src/DiscreteHyperbolicFactorizationUpperbound_TileSearch_Optimized_MultipleIntegers.py has been committed to iterate multiple integers sequentially over a range and time their computational geometric factorization durations by invoking python-src/DiscreteHyperbolicFactorizationUpperbound_TileSearch_Optimized.py within subprocess.call() 1205.2 NeuronRain Spark MapReduce implementation of Computational Geometric Factorization NC-PRAM-BSP algorithm by local binary search of rasterized hyperbolic arc bow segments is in complexity class Deterministic MRC, a software limitation. Time durations of computational geometric factorization of 300 integers from 100 to 400 and 20 integers from 500 to 520 are plotted against theoretical time of O(logN*logN) for depth-1 non-uniform Nick's Class circuit. 1205.3 python-src/DiscreteHyperbolicFactorizationUpperbound_TileSearch_Optimized_MultipleIntegers.py takes following commandline arguments in the order - minimum integer of the range,increment,depth of non-uniform NC circuit=exponent k of O((logN)^k),constant of Big-O and exponent of logN in the plot. 1205.4 It is hereby clarified that motive of Computational Geometric Factorization algorithm is to devise a polylogarithmic time integer factorization mechanism or a logdepth bounded fanin Nick's class circuit implemented by a cloud computing DMRC simulation of multicore-PRAMs which evidences existence of factorization algorithms in NC and not proving the lowerbound for factorization (e.g possibility of factorization by planar point location being present in a subset of NC cannot be ruled out - sublogarithmic time - since there are sequential sublogarithmic planar point location algorithms) which are two entirely different notions. That is precisely the reason for (ab)use of BigO notation throughout instead of Omega. 1205.5 Equivalence of SAC=AC=NC=TC implies Nick's class parallel algorithms are learnable by Threshold circuits of majority gates or Neural networks - iterated factorization of sequential set of integers might serve as priors for further factorization. Example: Factorization of 39 as 39 = 13*3 might be a prior by substitution for factorization of 117=3*39=3*3*13 1205.6 Two matlibplots python-src/DiscreteHyperbolicFactorizationUpperbound_TileSearch_Optimized_MultipleIntegers1.jpg (for integers 100 to 400,logdepth=1,BigOconstant=1,log exponent in plot=2) and python-src/DiscreteHyperbolicFactorizationUpperbound_TileSearch_Optimized_MultipleIntegers2.jpg (for integers 500 to 520,logdepth=1,BigOconstant=1,log exponent in plot=2) have been committed to AstroInfer repositories. 1205.7 Both the graphics depict the k*logN (k=1) actual runtimes of factorization (blue) far upperbounded by theoretical O((logN)^2) (red). Depth of circuit (parallel RAM time) has been differentiated from exponent of logarithm for graphic illustration (though both have to be equal by definition of NC). Previous runtime plots are for finding all factors (with and without multiplicity - Big Omega and Little Omega) which is a harder problem than finding just one factor.

---
+++ python-src/DiscreteHyperbolicFactorizationUpperbound_TileSearch_Optimized_MultipleIntegers.py
@@ -0,0 +1,50 @@
+# -------------------------------------------------------------------------------------------------------
+# ASFER - Software for Mining Large Datasets
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------------------------------
+# K.Srinivasan
+# NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+# Personal website(research): https://acadpdrafts.readthedocs.io/en/latest/
+# --------------------------------------------------------------------------------------------------------
+
+import math
+import sys
+from complement import toint
+import time
+import matplotlib.pyplot as plt
+import subprocess
+
+if __name__=="__main__":
+    mininteger=toint(sys.argv[1])
+    increment=toint(sys.argv[2])
+    actual_runtimes=[]
+    theoretical_runtimes=[]
+    depth=toint(sys.argv[3])
+    constant=toint(sys.argv[4])
+    exp=toint(sys.argv[5])
+    for n in range(mininteger,mininteger+increment):
+        print("================================================================================================")
+        print("Factorization of ",n, " (",math.log(n,2)," bit integer) ")
+        print("================================================================================================")
+        starttime=time.time()
+        subprocess.call(["/home/ksrinivasan/spark-3.0.1-bin-hadoop3.2/bin/spark-submit",
+                     "DiscreteHyperbolicFactorizationUpperbound_TileSearch_Optimized.py", str(n), str(depth), "False"], shell=False)
+        endtime=time.time()
+        duration=endtime-starttime
+        actual_runtimes.append(duration)
+        print("Time - Factorization of ",n, " (",math.log(n,2)," bit integer) was done in time deltas:",duration)
+        theoretical=constant*math.pow(math.log(n,2),exp)
+        print("Time - Theoretical Factorization of ",n, " (",math.log(n,2)," bit integer) time :",theoretical)
+        theoretical_runtimes.append(theoretical)
+    plt.plot(range(increment),actual_runtimes)
+    plt.plot(range(increment),theoretical_runtimes)
+    plt.show()
