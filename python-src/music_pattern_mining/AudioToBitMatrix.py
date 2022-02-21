c4218705ab8c1079cdb016ecd1f323845c8057e7
Krishna iResearch - NeuronRain - K.Srinivasan <ka.shrinivaasan@gmail.com>
2022-02-21 09:38:13 +0530

    ---------------------------------------------------------------------------------------------------------------------------------------------- 1218. (THEORY and FEATURE) Music Synthesizer - Python 3.8 upgrade and Fractal implementation - 21 February 2022 - related to 642,643,644,659,1124 and all sections on Intrinsic Merit of Music, MIR(Music Information Retrieval), Music Similarity and Clustering, AI Music Synthesis, fMRI connectomes - OpenNeuro - Psychology of music, Weighted automata, Chaos, Deep Learning --------------------------------------------------------------------------------------------------------------------------------------------- 1218.1 Music Synthesizer in python-src/music_pattern_mining/AudioToBitMatrix.py (from Functions,Automata and Randomness) has been enhanced to generate music notes waveform from a fractal dataset. It has also been upgraded to Python 3.8 by 2to3 and autopep8. 1218.2 Function notes_to_audio() has been updated to include additional boolean argument for enabling fractal waveform and a clause for computing the chaotic Mandelbrot set (Real only) from a logistic function. Samplerate and Amplitude for the waveform have been set accordingly for loading .wav file in Audacity. Fractal set of notes is grown within a loop of eval() by feedback. 1218.3 Fractal music waveform is written to separate .wav file - function_synthesized_music_fractal.wav. Scaled notes are cast to NumPy int16. 1218.4 Amplitude is multiplied by array of notes by NumPy Array broadcast throughout for all types of music synthesis - function,fractal,automata and randomness. Music waveform has been sufficiently amplified now and audible in Audacity. Waveform could be loaded in Audacity and analyzed visually by zooming the track. 1218.5 Three invocations of music synthesizer below respectively generate music from a quadratic equation, sinusoid and a Fractal Verhulste-Pearl-Reed(VPR) logistic function :         notes_to_audio(function='(x*x+x+1) % 32767',fractal=False)         notes_to_audio(function='int(math.sin(x*x+x+1) * 32767)',fractal=False)         notes_to_audio(function='5*(x*x-x) % 32767') 1218.6 Following are logs and music waveforms synthesized from finite state automaton,function,randomness and fractals:         automaton_synthesized_music.wav         function_synthesized_music.wav         notes_synthesized_music.wav         function_synthesized_music_fractal.wav         testlogs/AudioToBitMatrix.log.MusicSynthesizer.21February2022
    References:
    ----------
    1218.7 Deep Learning MIR - https://ccrma.stanford.edu/workshops/deepmir-2017
    1218.8 DeepMIR 2017 Workshop material - https://github.com/bmcfee/stanford-mir

--- python-src/music_pattern_mining/AudioToBitMatrix.py
+++ python-src/music_pattern_mining/AudioToBitMatrix.py
@@ -13,10 +13,11 @@
 # --------------------------------------------------------------------------------------------------------
 # K.Srinivasan
 # NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
-# Personal website(research): https://sites.google.com/site/kuja27/
+# Personal website(research): https://acadpdrafts.readthedocs.io/en/latest/
 # --------------------------------------------------------------------------------------------------------
 
 import librosa
+import math
 import numpy
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,7 +26,7 @@ import ast
 from sklearn.preprocessing import scale
 from scipy.spatial.distance import directed_hausdorff
 from scipy.stats import wasserstein_distance
-from RecursiveLambdaFunctionGrowth import RecursiveLambdaFunctionGrowth
+#from RecursiveLambdaFunctionGrowth import RecursiveLambdaFunctionGrowth
 
 # states2notes_machine={'s1-s2':'C','s2-s1':'E','s2-s3':'D','s3-s2':'G','s3-s4':'E','s4-s5':'F','s1-s3':'G','s4-s6':'A','s5-s6':'B','s4-s3':'F','s6-s5':'E','s3-s6':'A','s6-s1':'B'}
 
@@ -46,7 +47,7 @@ def audio_to_bitmatrix(audio, dur=None, binary=False):
         for r in input_audio_array:
             bitmap.append(bin(r))
 
-    #print "audio_to_bitmatrix() for - ",audio,":",bitmap
+    # print "audio_to_bitmatrix() for - ",audio,":",bitmap
     return (bitmap, waveform, srate)
 
 
@@ -55,11 +56,13 @@ def audio_distance(audio1, audio2, dur=10):
     bitmap2, waveform2, srate2 = audio_to_bitmatrix(audio2, dur)
     print(("waveform1:", waveform1))
     print(("waveform2:", waveform2))
-    hausdorff_distance = directed_hausdorff([waveform1],[waveform2]) 
-    emd_distance = wasserstein_distance(waveform1,waveform2) 
-    print("Hausdorff Distance similarity between two audio waveforms:",hausdorff_distance)
-    print("Earth Mover Distance similarity between two audio waveforms:",emd_distance)
-    return (hausdorff_distance,emd_distance)
+    hausdorff_distance = directed_hausdorff([waveform1], [waveform2])
+    emd_distance = wasserstein_distance(waveform1, waveform2)
+    print(("Hausdorff Distance similarity between two audio waveforms:",
+          hausdorff_distance))
+    print(("Earth Mover Distance similarity between two audio waveforms:", emd_distance))
+    return (hausdorff_distance, emd_distance)
+
 
 def speechrecognition_audiograph(audiofile):
     import speech_recognition as spreg
@@ -67,7 +70,7 @@ def speechrecognition_audiograph(audiofile):
     with spreg.AudioFile(audiofile) as audiosource:
         audiodata = recog.record(audiosource)
     recogspeech = recog.recognize_sphinx(audiodata)
-    print("Recognized Speech:", recogspeech)
+    print(("Recognized Speech:", recogspeech))
     print("Graph Tensor Neuron Network Merit of Recognized Speech:")
     rlfg = RecursiveLambdaFunctionGrowth()
     rlfg.grow_lambda_function3(recogspeech)
@@ -78,9 +81,9 @@ def audio_features(signal_bitmap):
     print("Histogram/Probability Distribution of the audio signal")
     print("################################################")
     hist, bin = np.histogram(signal_bitmap[0], density=True)
-    print("hist:", hist)
-    print("bin:", bin)
-    #plt.hist(signal_bitmap, color='r', range=(0, 0.2), alpha=0.5, bins=20)
+    print(("hist:", hist))
+    print(("bin:", bin))
+    # plt.hist(signal_bitmap, color='r', range=(0, 0.2), alpha=0.5, bins=20)
     # plt.show()
     print("#################################################")
     print("Note Onset Detection")
@@ -91,7 +94,7 @@ def audio_features(signal_bitmap):
         np.arange(len(onstrength)), sr=signal_bitmap[2])
     onset_frames = librosa.onset.onset_detect(
         onset_envelope=onstrength, sr=signal_bitmap[2])
-    print("Notes onsets occur at:", onset_frames)
+    print(("Notes onsets occur at:", onset_frames))
 
     return (hist, bin, times, onstrength, onset_frames)
 
@@ -103,39 +106,55 @@ def audio_to_notes(audio, dur=None):
     if dur is not None:
         waveform, srate = librosa.load(audio, duration=dur)
     freq = np.abs(librosa.stft(waveform))
-    print("Frequencies:", freq)
+    print(("Frequencies:", freq))
     notes = librosa.hz_to_note(freq)
-    print("Notes:", notes)
+    print(("Notes:", notes))
     return notes
 
 
-def notes_to_audio(automaton=False, function=None, deterministic=True, maxsamplesize=44100):
+def notes_to_audio(automaton=False, function=None, deterministic=True, samplerate=44100, fractal=True):
+    amplitude = np.iinfo(np.int16).max
     if function != None:
         print("###################################################")
         print("Function to Audio")
         print("###################################################")
-        print("Function:", function)
+        print(("Function:", function))
         # Example:
         # >>> map(lambda x: eval('x*x+x+1'),range(1,10))
-        #[3, 7, 13, 21, 31, 43, 57, 73, 91]
-        notes = [eval(function) for x in range(0, 44100)]
+        # [3, 7, 13, 21, 31, 43, 57, 73, 91]
+        notes = []
+        if fractal:
+            function_nplus1 = 2
+            for y in range(0, samplerate*10):
+                x = function_nplus1
+                function_nplus1 = eval(function)
+                notes.append(function_nplus1)
+                #print("function_nplus1:", function_nplus1)
+        else:
+            notes = [amplitude*eval(function) for x in range(0, samplerate*10)]
         npnotes = np.asarray(notes)
-        # scalednpnotes=np.int16(npnotes/np.max(npnotes)*32767)
+        #scalednpnotes = np.int16(npnotes/np.max(npnotes)*32767)
         scalednpnotes = npnotes
-        print("Notes :", scalednpnotes)
-        print("Size of scaled notes:", len(scalednpnotes))
-        write("function_synthesized_music.wav", maxsamplesize, scalednpnotes)
+        print(("Notes :", scalednpnotes))
+        print(("Size of scaled notes:", len(scalednpnotes)))
+        if fractal:
+            write("function_synthesized_music_fractal.wav",
+                  samplerate, scalednpnotes.astype(np.int16))
+        else:
+            write("function_synthesized_music.wav",
+                  samplerate, scalednpnotes.astype(np.int16))
         return
     if function == None and automaton == False:
         print("###################################################")
         print("Notes to Audio")
         print("###################################################")
-        npnotes = np.random.uniform(10, 100, 44100)
+        npnotes = amplitude*np.random.uniform(10, 100, 44100)
         # scalednpnotes=np.int16(npnotes/np.max(npnotes)*32767)
         scalednpnotes = npnotes
-        print("Notes :", scalednpnotes)
-        print("Size of scaled notes:", len(scalednpnotes))
-        write("notes_synthesized_music.wav", maxsamplesize, scalednpnotes)
+        print(("Notes :", scalednpnotes))
+        print(("Size of scaled notes:", len(scalednpnotes)))
+        write("notes_synthesized_music.wav", samplerate,
+              scalednpnotes.astype(np.int16))
         return
     if automaton == True:
         print("###################################################")
@@ -148,14 +167,14 @@ def notes_to_audio(automaton=False, function=None, deterministic=True, maxsample
             states2notes_machine['start-s1'])*1000)]
         prevstates = ['start']
         iter = 0
-        while iter < maxsamplesize-1:
+        while iter < samplerate*10:
             possibletransitions = []
             prevprevstates = prevstates
             prevstates = []
-            #print "prevstate:",prevstate
+            # print "prevstate:",prevstate
             # if 'fs' in prevstate:
             #	break
-            for k, v in states2notes_machine.items():
+            for k, v in list(states2notes_machine.items()):
                 statetransition = k.split("-")
                 if statetransition[0] in prevprevstates:
                     possibletransitions.append(states2notes_machine[k])
@@ -164,16 +183,17 @@ def notes_to_audio(automaton=False, function=None, deterministic=True, maxsample
                         break
             for note in possibletransitions:
                 hertz = librosa.note_to_hz(note)
-                #print "Hertz:",hertz
-                dfanotes.append(int(hertz*1000))
+                # print "Hertz:",hertz
+                dfanotes.append(amplitude*int(hertz*1000))
                 # break
             iter += 1
         npnotes = np.array(dfanotes)
         # scalednpnotes=np.int16(npnotes/np.max(npnotes)*32767)
         scalednpnotes = npnotes
-        print("Notes :", scalednpnotes)
-        print("Size of scaled dfanotes:", len(scalednpnotes))
-        write("automaton_synthesized_music.wav", maxsamplesize, scalednpnotes)
+        print(("Notes :", scalednpnotes))
+        print(("Size of scaled dfanotes:", len(scalednpnotes)))
+        write("automaton_synthesized_music.wav",
+              samplerate, scalednpnotes.astype(np.int16))
         return
 
 
@@ -184,36 +204,36 @@ def mel_frequency_cepstral_coefficients(audiofile, dur=10):
     waveform, srate = librosa.load(audiofile, duration=dur)
     mfccs = librosa.feature.mfcc(waveform, sr=srate)
     mfccs = scale(mfccs, axis=1)
-    print("mfccs:", mfccs)
-    print("mfccs shape:", mfccs.shape)
-    print("mfccs mean:", mfccs.mean(axis=1))
-    print("mfccs variance:", mfccs.var(axis=1))
-    print("zero crossing rate:", librosa.feature.zero_crossing_rate(waveform))
+    print(("mfccs:", mfccs))
+    print(("mfccs shape:", mfccs.shape))
+    print(("mfccs mean:", mfccs.mean(axis=1)))
+    print(("mfccs variance:", mfccs.var(axis=1)))
+    print(("zero crossing rate:", librosa.feature.zero_crossing_rate(waveform)))
     return (mfccs, mfccs.shape, mfccs.mean(axis=1), mfccs.var(axis=1))
 
 
 def audio_merit(notes):
     from MinimumDescLength import minimum_descriptive_complexity
     entropy_merit = minimum_descriptive_complexity("".join(notes))
-    print("Merit of Audio - Minimum Descriptive Length and Entropy:", entropy_merit)
+    print(("Merit of Audio - Minimum Descriptive Length and Entropy:", entropy_merit))
 
 
 if __name__ == "__main__":
     # bm=mel_frequency_cepstral_coefficients("./testlogs/JSBach_Musicological_Offering.mp4",dur=20)
     # speechrecognition_audiograph("testlogs/AudioGraphExample_SpeechRecognition_2019-07-09-103018.wav")
-    audio_distance("./testlogs/JSBach_Musicological_Offering.mp4",
-                   "./testlogs/JSBach_Musicological_Offering.mp4")
-    audio_distance("./testlogs/JSBach_Musicological_Offering.mp4",
-                    "./testlogs/AudioGraphExample_SpeechRecognition_2019-07-09-103018.wav")
+    # audio_distance("./testlogs/JSBach_Musicological_Offering.mp4", "./testlogs/JSBach_Musicological_Offering.mp4")
+    # audio_distance("./testlogs/JSBach_Musicological_Offering.mp4", "./testlogs/AudioGraphExample_SpeechRecognition_2019-07-09-103018.wav")
     # bm=audio_to_bitmatrix("/media/Krishna_iResearch_/Krishna_iResearch_OpenSource/GitHub/asfer-github-code/python-src/music_pattern_mining/testlogs/JSBach_Musicological_Offering.mp4",dur=20)
-    #bm=audio_to_bitmatrix("/media/Krishna_iResearch_/Krishna_iResearch_OpenSource/GitHub/asfer-github-code/python-src/music_pattern_mining/testlogs/Bach Sonata No 2.mp3",dur=10)
+    # bm=audio_to_bitmatrix("/media/Krishna_iResearch_/Krishna_iResearch_OpenSource/GitHub/asfer-github-code/python-src/music_pattern_mining/testlogs/Bach Sonata No 2.mp3",dur=10)
     # bm=audio_to_bitmatrix("/media/Krishna_iResearch_/Krishna_iResearch_OpenSource/GitHub/asfer-github-code/python-src/DFT_multimedia_HilbertRadioAddress.mp3.mpga",dur=10)
-    #print "Bitmap:",bm[0]
+    # print "Bitmap:",bm[0]
     # features=audio_features(bm)
-    #print "Features:",features
-    #notes=audio_to_notes("/media/Krishna_iResearch_/Krishna_iResearch_OpenSource/GitHub/asfer-github-code/python-src/music_pattern_mining/testlogs/Bach Sonata No 2.mp3",dur=10)
+    # print "Features:",features
+    # notes=audio_to_notes("/media/Krishna_iResearch_/Krishna_iResearch_OpenSource/GitHub/asfer-github-code/python-src/music_pattern_mining/testlogs/Bach Sonata No 2.mp3",dur=10)
     # notes=audio_to_notes("/media/Krishna_iResearch_/Krishna_iResearch_OpenSource/GitHub/asfer-github-code/python-src/music_pattern_mining/testlogs/JSBach_Musicological_Offering.mp4",dur=20)
     # merit=audio_merit(notes[0])
-    # notes_to_audio()
-    # notes_to_audio(automaton=True)
-    #notes_to_audio(function='(x*x+x+1) % 32767')
+    notes_to_audio()
+    notes_to_audio(automaton=True)
+    notes_to_audio(function='(x*x+x+1) % 32767',fractal=False)
+    #notes_to_audio(function='int(math.sin(x*x+x+1) * 32767)',fractal=False)
+    notes_to_audio(function='5*(x*x-x) % 32767')
