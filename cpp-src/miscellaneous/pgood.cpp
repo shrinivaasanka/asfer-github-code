471583cd8ad3232eb48fe30f6130a09769c405b1
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2023-05-18 13:27:17 +0530

    -------------------------------------------------------------------------------------------------------------------------------------------- 1426. (THEORY and FEATURE) Asymptotic approximation of Condorcet Jury Theorem - C++ implementation - related to 400,572,666,725,730,688,1136,1137,1152 and all sections on Intrinsic Merit Versus Majority Voting, Derandomization, P=BPP ("Maybe" tends to "Exists") in the context of Condorcet Jury Theorem and its derivatives, Margulis-Russo Threshold for boolean majority, Histogram analytics, Set Partition Theoretical Electronic Voting Machines, Edit distance to Earth Mover Distance reduction, Boolean function composition, KRW Conjecture, Communication complexity, Hardness amplification, Bounded Rationality, Gibbard-Satterthwaite Theorem, Ramsey coloring of sequences, Infinite Pigeonhole Principle, Voting Analytics (pre-poll and post-poll),Epsilon bias spaces - 18 May 2023 -------------------------------------------------------------------------------------------------------------------------------------------- 1426.1 cpp-src/miscellaneous/pgood.cpp has been updated to include <cmath> headers and has been enhanced to implement a new function condorcet_jury_theorem() which computes asymptotic appoximation of CJT when p is very close to 0.5 or voters are epsilon-bias spaces. New flag iterative chooses either the binomial series iterative evaluation of CJT or asymptotic approximation of CJT from expression mentioned in https://en.wikipedia.org/wiki/Condorcet%27s_jury_theorem#Asymptotics based on user input of voter size n,probability p and constants c2,c3,c4. 1426.2 Condorcet Jury Theorem is central to lot of majority voting consensus related theory mentioned in NeuronRain design and is used as asymptotic derandomization gadget for BP* classes, particularly asymptotic derandomization of Shor BQP factorization by majority voting in section 1152 (https://neuronrain-documentation.readthedocs.io/en/latest/) in which every BPQ voter factorizes an integer with 67% accuracy and by CJT p-bias=0.67 making the group decision tend to 100% correctness asymptotically (Margulis Ruzzo Sharp Threshold). 1426.3 Some example asymptotic approximation of CJT for various population sizes and probabilities have been captured in cpp-src/miscellaneous/testlogs/pgood.log.18May2023
    References:
    ---------
    1426.4 When is Condorcet Jury Theorem Valid - https://www.jstor.org/stable/41106274
    1426.5 On the probability of the Condorcet Jury Theorem or the Miracle of Aggregation - https://www.sciencedirect.com/science/article/abs/pii/S0165489622000543?via%3Dihub
    1426.6 The Random Forest Algorithm explained by Condorcet Jury Theorem - https://mlu-explain.github.io/random-forest/
    1426.7 Three Jury Theorems - https://plato.stanford.edu/entries/jury-theorems/
    1426.8 MAJORITY VOTING AND THE CONDORCET’S JURY THEOREM - https://arxiv.org/pdf/2002.03153.pdf
    1426.9 Condorcet Jury Theorem - https://en.wikipedia.org/wiki/Condorcet%27s_jury_theorem
    1426.10 Small Bias Sample Spaces - https://en.wikipedia.org/wiki/Small-bias_sample_space - Voter vector is defined as string of binary decisions by voters (E.g Good-Good-Bad-Bad-Good is a vote vector for decision correctness of 5 voters). Bias is the difference between parity Bad or 0 (Overall bad group decision of a vote vector) and parity Good or 1 (Overall good group decision of a vote vector) - Small bias spaces fool parity - Earlier asymptotic approximation is for p closer to 0.5 (e.g 0.49999999) giving rise to voter sample space that is too close to call.

--- cpp-src/miscellaneous/pgood.cpp
+++ cpp-src/miscellaneous/pgood.cpp
@@ -1,31 +1,21 @@
 /***************************************************************************************
-ASFER - a ruleminer which gets rules specific to a query and executes them
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
- ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
----------------------------------------------------------------------------------------------------
-Copyright (C):
-Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
-Independent Open Source Developer, Researcher and Consultant
-Ph: 9789346927, 9003082186, 9791165980
-Open Source Products Profile(Krishna iResearch): http://sourceforge.net/users/ka_shrinivaasan
-Personal website(research): https://sites.google.com/site/kuja27/
-emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com, kashrinivaasan@live.com
----------------------------------------------------------------------------------------------------
-*****************************************************************************************/
-
-/*
+# -------------------------------------------------------------------------------------------------------
+# NEURONRAIN ASFER - Software for Mining Large Datasets
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------------------------------
+# K.Srinivasan
+# NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+# Personal website(research): https://acadpdrafts.readthedocs.io/en/latest/ 
+# --------------------------------------------------------------------------------------------------------
 #####################################################################################
  Old CPP test code written in 2006 for deriving error probability of majority voting
  and used 3 years ago in January 2010 during MSc thesis at IIT Chennai
@@ -37,7 +27,8 @@ http://sourceforge.net/projects/acadpdrafts/files/MScThesis-Writeup-Complete.pdf
 For publications:
  1. http://arxiv.org/abs/1006.4458
  2. http://www.nist.gov/tac/publications/2010/participant.papers/CMI_IIT.proceedings.pdf
- and other publication drafts for majority voting BPNC circuits in https://sites.google.com/site/kuja27/
+and other publication drafts related to majority voting and derandomization of BP* classes in 
+https://acadpdrafts.readthedocs.io/en/latest/ (especially CJT derandomization gadget in section 1152)
 #####################################################################################
 */
 
@@ -63,6 +54,7 @@ Thus with zero error both pseudorandom choice and majority vote yield P(good)= 1
 using namespace std;
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 extern "C"
 {
@@ -71,31 +63,63 @@ extern "C"
 
 long double factorial(long double);
 long double power_of_4(long double);
+long double condorcet_jury_theorem(long double n,long double p,long double c2,long double c3,long double c4);
+bool iterative=false;
 
 int main()
 {
 /*
 P(good) = (2n)!/(4^n) { 1/(n+1)!(n-1)! + 1/(n+2)!(n-2)! + ... + 1/(n+n)!(n-n)!}
 */
-	long double n = 0.0, i=0.0;
-	long double prevsum = 0.0, sum = 0.0, prevsumdiff = 0.0, sumdiff = 0.0, term1 = 0.0;
-	for(n = 0.0; n <= 30000.0; n++)
+	if(iterative)
 	{
-		term1 = factorial(2*n) / power_of_4(n);
-		for (i=1.0; i <= n ; i++)
-			sum = sum + (1.0 / (factorial(n+i) * factorial(n-i)));
-		sum = term1 * sum;
-		cout << "Probability of good choice for population of " << 2*n << "=" << sum*100.0 <<endl;
-		sumdiff = sum - prevsum;
-		cout << "prob - prevprob = " << sumdiff << endl;
-		cout << "Convergence test: (sum - prevsum)/prevsum = " << sumdiff/prevsum << endl;
-		prevsum = sum;
-		prevsumdiff = sumdiff;
-		sum =0.0;
+		long double n = 0.0, i=0.0;
+		long double prevsum = 0.0, sum = 0.0, prevsumdiff = 0.0, sumdiff = 0.0, term1 = 0.0;
+		for(n = 0.0; n <= 30000.0; n++)
+		{
+			term1 = factorial(2*n) / power_of_4(n);
+			for (i=1.0; i <= n ; i++)
+				sum = sum + (1.0 / (factorial(n+i) * factorial(n-i)));
+			sum = term1 * sum;
+			cout << "Probability of good choice for population of " << 2*n << "=" << sum*100.0 <<endl;
+			sumdiff = sum - prevsum;
+			cout << "prob - prevprob = " << sumdiff << endl;
+			cout << "Convergence test: (sum - prevsum)/prevsum = " << sumdiff/prevsum << endl;
+			prevsum = sum;
+			prevsumdiff = sumdiff;
+			sum =0.0;
+		}
+	}
+	else
+	{
+		long double n;
+		long double p;
+		long double c2;
+		long double c3;
+		long double c4;
+		cout<<"n:"<<endl;
+		cin>>n;
+		cout<<"p:"<<endl;
+		cin>>p;	
+		cout<<"c2 (0 to 1):"<<endl;
+		cin>>c2;
+		cout<<"c3 (0 to 1):"<<endl;
+		cin>>c3;
+		cout<<"c4 (0 to 1):"<<endl;
+		cin>>c4;
+		condorcet_jury_theorem(n,p,c2,c3,c4);
 	}
 
 }
 
+long double condorcet_jury_theorem(long double n,long double p,long double c2,long double c3,long double c4)
+{
+	long double c1 = sqrt((2*n + 1)/3.14159265358979323846)*(1 + 1/(16*pow(n,2)) + c2/(pow(n,3))); 
+	cout<<"c1 computed:"<<c1<<endl;
+	long double cjt = 0.5 + c1*(p-0.5) + c3*pow(p-0.5,3) + c4*(p-0.5,5);
+	cout << "Condorcet Jury Theorem asymptotic approximation for n="<<n<<" and p="<<p<<":"<<cjt<<endl;
+	return cjt;
+}
 
 long double factorial(long double n)
 {
