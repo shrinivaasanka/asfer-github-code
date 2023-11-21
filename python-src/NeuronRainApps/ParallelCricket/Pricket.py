10c68f05dc19ca1cd5960337bab59c831b09c49a
K.Srinivasan <ksrinivasan@krishna-iresearch.org>
2023-11-21 16:00:54 +0530

    ------------------------------------------------------------------------------------------------------------------------------- 1460. (THEORY and FEATURE) NeuronRainApps - Sports Analytics - Sabermetrics - Concept Implementation of Parallel Cricket (PRICKET) simulator - related to 754 and all sections on Intrinsic Performance Ratings and Sports Analytics, Streaming Histogram analytics, Partition distance measures, Random graph streams, Logic and Game theory, Generalized Geography and PSPACE-completeness, Parity games - 21 November 2023 ------------------------------------------------------------------------------------------------------------------------------- 1460.1 New source file python-src/NeuronRainApps/ParallelCricket/Pricket.py has been added to NeuronRain AstroInfer repositories which conceptually and minimally implements parallel or semisequential version of the game of cricket (described in Section 754) which is sequentially played (one team chasing the target set by the other). 1460.2 python-src/NeuronRainApps/ParallelCricket/Pricket.py instantiates a Player object defined by class of same name. Player class also defines a function play_shot() for a ball which is simulated by a random angle of the drive played by the batter on the outfield. Outfield of 360 degrees is split into 10 sections of 36 degrees each one per fielder. Ball is deemed to be caught or fielded if the trajectory of the ball is outside (lowerdegree,upperdegree) range for lowerdegree > 0 and upperdegree < 36 i.e 2 adjoining fielders in outfield are able to reach the ball within lowerdegree or upperdegree in two directions. Runs per ball is also chosen at random from an enumerated array [0,1,2,3,4,6] of possible runs per shot. 1460.3 Function def pricket_sabermetrics(number_of_balls=6,team1=None,team2=None,lowerdegree=3,upperdegree=33) loops through 2 deques for team1 and team2 each containing 11 Player objects and invokes play_shot() twice for one player each from both teams thus parallely enabling both teams to bat and bowl by alternate role reversals every second ball. Players are disqualified if shot of the angle is outside predefined degree range. Game stops when either of the team deque is empty by allout. 1460.4 Earlier implementation dynamically traces a Generalized Geography kind-of graph, where playerY of team2 continues from where playerX of team1 stopped in previous ball (and viceversa) thus reducing Cricket to GG in parallel sense. 1460.5 In PRICKET, probability of a player getting out = (36 - upperdegree + lowerdegree)/36. An example commentary transcript of PRICKET for 300 balls and 2 teams of 11 players each is at python-src/NeuronRainApps/ParallelCricket/testlogs/Pricket.log.21November2023 : 	Team2 allout - Number of balls bowled: 300 	Team1 runs: 309 	Team2 runs: 360 	Team2 wins 1460.6 Multiple scenarios can be simulated by varying the lower and upper degree range for wicket loss. Though a parallel cricket is practically less feasible as fielding-batting sides have to be interchanged every ball, virtual parallel software version of the game is lot more flexible as role alternation every ball is programmable. In parallel cricket, there is no predefined target to chase as both teams are simultaneously at the crease. Present implementation does not cover all possible "out" scenarios - clean bowled, caught and bowled, stumped, runout, LBW are yet to be implemented.
    References:
    ----------
    1460.7 Parity game - https://en.wikipedia.org/wiki/Parity_game - Soccer can be reduced to Parity game on a dynamic graph in which two sets of nodes (Square and Circular) simulate 2 teams and the token passed between the nodes on a dynamic graph is the ball with 2 winning regions as goalposts. Finding an algorithm for solving Parity games in polynomial time is an open problem.

---
+++ python-src/NeuronRainApps/ParallelCricket/Pricket.py
@@ -0,0 +1,134 @@
+# -------------------------------------------------------------------------------------------------------
+# NEURONRAIN ASFER - Software for Mining Large Datasets
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------------------------------
+# Copyleft (Copyright+):
+# NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+# Personal website(research): https://acadpdrafts.readthedocs.io/en/latest/
+# --------------------------------------------------------------------------------------------------------
+
+from collections import deque
+import random
+
+run_enum=[0,1,2,3,4,6]
+
+class Player(object):
+    def __init__(self,name,teamid):
+        self.id = name
+        self.teamid = teamid 
+        self.state = "Playing"
+        self.runs = 0
+    
+    def play_shot(self,ball):
+        angle_of_the_shot = random.randint(1,360) % 36 
+        run = run_enum[random.randint(0,len(run_enum)-1)] 
+        self.runs += run
+        print("Ball - ",ball," - Player:",self.id," - Team:",self.teamid," - angle of the shot (in degrees):",angle_of_the_shot)
+        print("Ball - ",ball," - Player:",self.id," - run scored:",run)
+        return (angle_of_the_shot,run)
+
+def print_deque(deq):
+    deqstr=[]
+    for e in deq:
+        deqstr.append(e.id)
+    return deqstr
+
+def pricket_sabermetrics(number_of_balls=6,team1=None,team2=None,lowerdegree=3,upperdegree=33):
+    ball=1
+    player1 = team1.popleft()
+    player2 = team2.popleft()
+    team1_runs = 0
+    team2_runs = 0
+    while ball <= number_of_balls:
+        shot = player1.play_shot(ball)
+        if shot[0] < lowerdegree or shot[0] > upperdegree:
+            player1.state = "Out"
+            print("Shot outside [",lowerdegree," to ",upperdegree,"] degrees range - Player ",player1.id," of team ",player1.teamid," is Out")
+            if len(team1) > 0:
+                player1 = team1.popleft()
+                print("Team1 - yet to bat:",print_deque(team1))
+            else:
+                print("Team1 allout - Number of balls bowled:",ball)
+                exit
+        else:
+            team1_runs += shot[1] 
+        ball+=1
+        shot = player2.play_shot(ball)
+        if shot[0] < lowerdegree or shot[0] > upperdegree:
+            player2.state = "Out"
+            print("Shot outside [",lowerdegree," to ",upperdegree,"] degrees - Player ",player2.id," of team ",player2.teamid," is Out")
+            if len(team2) > 0:
+                player2 = team2.popleft()
+                print("Team2 - yet to bat:",print_deque(team2))
+            else:
+                print("Team2 allout - Number of balls bowled:",ball)
+                exit
+        else:
+            team2_runs += shot[1] 
+        ball+=1
+    print("Team1 runs:",team1_runs)
+    print("Team2 runs:",team2_runs)
+    if team1_runs > team2_runs:
+        print("Team1 wins")
+    if team2_runs > team1_runs:
+        print("Team2 wins")
+    if team1_runs == team2_runs:
+        print("Team1 and Team2 tied")
+
+if __name__=="__main__":
+    p1=Player("player1","team1")
+    p2=Player("player2","team1")
+    p3=Player("player3","team1")
+    p4=Player("player4","team1")
+    p5=Player("player5","team1")
+    p6=Player("player6","team1")
+    p7=Player("player7","team1")
+    p8=Player("player8","team1")
+    p9=Player("player9","team1")
+    p10=Player("player10","team1")
+    p11=Player("player11","team1")
+    p12=Player("player12","team2")
+    p13=Player("player13","team2")
+    p14=Player("player14","team2")
+    p15=Player("player15","team2")
+    p16=Player("player16","team2")
+    p17=Player("player17","team2")
+    p18=Player("player18","team2")
+    p19=Player("player19","team2")
+    p20=Player("player20","team2")
+    p21=Player("player21","team2")
+    p22=Player("player22","team2")
+    team1=deque()
+    team2=deque()
+    team1.append(p1)
+    team1.append(p2)
+    team1.append(p3)
+    team1.append(p4)
+    team1.append(p5)
+    team1.append(p6)
+    team1.append(p7)
+    team1.append(p8)
+    team1.append(p9)
+    team1.append(p10)
+    team1.append(p11)
+    team2.append(p12)
+    team2.append(p13)
+    team2.append(p14)
+    team2.append(p15)
+    team2.append(p16)
+    team2.append(p17)
+    team2.append(p18)
+    team2.append(p19)
+    team2.append(p20)
+    team2.append(p21)
+    team2.append(p22)
+    pricket_sabermetrics(number_of_balls=300,team1=team1,team2=team2)
