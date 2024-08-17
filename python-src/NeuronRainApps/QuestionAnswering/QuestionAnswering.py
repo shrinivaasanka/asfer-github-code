6523839446337f193b63a75dcb2d65ca16ca40a0
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2024-08-18 01:41:13 +0530

    (*) Fix for https://krishnairesearch.atlassian.net/browse/NEUR-49 (*) Additional logs for blanks in sentence synthesis

--- python-src/NeuronRainApps/QuestionAnswering/QuestionAnswering.py
+++ python-src/NeuronRainApps/QuestionAnswering/QuestionAnswering.py
@@ -140,20 +140,19 @@ def WikipediaRLFGTransformersQuestionAnswering(question,questionfraction=1,maxan
                 numrw=0
                 sentences_synthesized=defaultdict(int)
                 random_walks = list(nx.generate_random_paths(answertextgraph[0].to_undirected(),number_of_random_walks,path_length=number_of_words_per_sentence))
+                corevertices = [x for x,y in answertextgraphclassified[0]] 
                 for random_walk in random_walks:
                     print("random walk:",random_walk)
                     core_random_walk_found=False
-                    while core_random_walk_found is not True:
-                        corevertices = [x for x,y in answertextgraphclassified[0]] 
-                        if len(set(random_walk).intersection(set(corevertices))) > number_of_cores_per_random_walk: 
-                                print("core_random_walk_found - random_walk:",random_walk)
-                                print("core_random_walk_found - corevertices:",corevertices)
-                                core_random_walk_found=True
-                                break
-                    if number_of_words_per_sentence==2:
-                        naturallanguageanswer = make_sentence(random_walk,sentence_type="xtag_node34_triplets")
-                    else:
-                        naturallanguageanswer = make_sentence(random_walk,sentence_type="textgraph_random_walk",standard_sentence_PoS_dict=std_sentence_PoS_dict,markblanks=blanks)
+                    if len(set(random_walk).intersection(set(corevertices))) > number_of_cores_per_random_walk: 
+                           print("core_random_walk_found - random_walk:",random_walk)
+                           print("core_random_walk_found - corevertices:",corevertices)
+                           core_random_walk_found=True
+                    if core_random_walk_found is True:
+                        if number_of_words_per_sentence==2:
+                             naturallanguageanswer = make_sentence(random_walk,sentence_type="xtag_node34_triplets")
+                        else:
+                             naturallanguageanswer = make_sentence(random_walk,sentence_type="textgraph_random_walk",standard_sentence_PoS_dict=std_sentence_PoS_dict,markblanks=blanks)
                         sentences_synthesized[numrw]=naturallanguageanswer
                     numrw+=1
                 print("Bot generated random walk answer from textgraph:")
@@ -214,5 +213,5 @@ if __name__ == "__main__":
     question = sys.argv[1]
     #WikipediaRLFGTransformersQuestionAnswering(question,bothvertices_intersection=True,sentence_type="xtag_node34_triplets")
     #WikipediaRLFGTransformersQuestionAnswering(question,bothvertices_intersection=False,sentence_type="xtag_node34_triplets")
-    WikipediaRLFGTransformersQuestionAnswering(question,wsheading=True,answerslice=0.01,bothvertices_intersection=False,sentence_type="textgraph_random_walk",number_of_words_per_sentence=50,std_sentence_PoS_dict={"ADJ":[],"PROPN":[],"NOUN":[],"AUX":[],"ADP":[],"ADV":[],"VERB":[],"DET":[],"PRON":[],"CCONJ":[],"NUM":[],"SYM":[],"X":[],"PUNCT":[]},number_of_cores_per_random_walk=3,number_of_random_walks=3,blanks=False)
+    WikipediaRLFGTransformersQuestionAnswering(question,wsheading=True,answerslice=0.01,bothvertices_intersection=False,sentence_type="textgraph_random_walk",number_of_words_per_sentence=50,std_sentence_PoS_dict={"ADJ":[],"PROPN":[],"NOUN":[],"AUX":[],"ADP":[],"ADV":[],"VERB":[],"DET":[],"PRON":[],"CCONJ":[],"NUM":[],"SYM":[],"X":[],"PUNCT":[]},number_of_cores_per_random_walk=3,number_of_random_walks=3,blanks=True)
     #OpenAIQuestionAnswering(question)
