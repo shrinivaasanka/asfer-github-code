a01cda4986f357e9310d4428564b99e383677c0c
K.Srinivasan <ksrinivasan@krishna-iresearch.org>
2024-09-30 14:37:11 +0530

    ------------------------------------------------------------------------------------------------------------------------------- 1553. (THEORY and FEATURE) Analysis of Matiyasevich Python programs for Riemann Hypothesis - Growth rate of while loop inequality - related to 629,751,1191,1217,1354,1357,1443,1459,1477,1552 to all sections on Riemann Zeta Function, Set partitions, Computational geometry, Program analysis/Software analytics/Scheduler Analytics,Master Theorem,Busy Beaver Function - 30 September 2024 ------------------------------------------------------------------------------------------------------------------------------- 1553.1 Python program 1 in reference 1552.14 - [Yu.Matiyasevich] has been added to NeuronRain AstroInfer repositories and is analyzed for the growth rate of LHS (p^2*(m-f0)) and RHS (f3) of while loop expression by additional computation. 1553.2 LHS of the while loop is complex compared to RHS and the values of LHS and RHS are plotted in matplotlib. 1553.3 Termination of while loop implies Riemann Hypothesis is False - Line g = gcd(n,q) has been changed to g = gcd(n,int(q)) due to python interpreter datatype cast error. 1553.4 An example execution of above program terminates after 52 iterations and growth of the while loop inequality LHS-RHS are captured in testlogs/MatiyasevichRH.LHS.30September2024.jpg ,  testlogs/MatiyasevichRH.log.30September2024 , testlogs/MatiyasevichRH.RHS.30September2024.jpg 1553.5 Termination of the loop after 50 iterations could be due to floating point precision errors - but the plots and logs show how fast the LHS and RHS of while loop grow (exponential - possibly in geometric progression) and there is a sharp threshold spike in both LHS and RHS after 50 iterations. 1553.6 Due to extraordinary growth of the while loop inequality in both sides, high precision Big decimal datatypes are mandatory further investigations are necessary to statically and dynamically analyze above program through available WCET tools including Heptane (1552.20) and aiT.

---
+++ python-src/MatiyasevichRH.py
@@ -0,0 +1,57 @@
+# -------------------------------------------------------------------------------------------------------
+# NEURONRAIN ASFER - Software for Mining Large Datasets
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------------------------------
+# K.Srinivasan
+# NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+# Personal website(research): https://acadpdrafts.readthedocs.io/en/latest/ 
+# --------------------------------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------------------------------------
+#Python code from https://www.sciencedirect.com/science/article/pii/S0304397519304633 - figure 2 - Program 1 -
+#[Yu.Matiyasevich] - The Riemann Hypothesis in computer science - additional statements included for WCET program analysis
+#--------------------------------------------------------------------------------------------------------------
+
+from math import gcd
+import matplotlib.pyplot as plt
+
+h=m=p=0
+d=f0=f3=n=q=1
+lhs=[]
+rhs=[]
+while p**2*(m-f0) < f3:
+    print("LHS:",p**2*(m-f0))
+    lhs.append(p**2*(m-f0))
+    print("RHS:",f3)
+    rhs.append(f3)
+    print("LHS/RHS:",p**2*(m-f0)/f3)
+    d = 2*n*d-4*(-1)**n*h
+    n = n+1
+    g = gcd(n,int(q))
+    print("g:",g)
+    q = n*q/g
+    print("q:",q)
+    if g==1: p=p+1
+    m=0; g=q
+    while g>1:
+        g=g//2; m=m+d
+    h=f0
+    f0=2*n*h
+    f3=(2*n+3)*f3
+print("Loop exits after ",n," iterations - Riemann Hypothesis is False")
+print("after loop - LHS:",p**2*(m-f0))
+print("after loop - RHS:",f3)
+print("after loop - LHS/RHS:",p**2*(m-f0)/f3)
+plt.plot(lhs)
+plt.show()
+plt.plot(rhs)
+plt.show()
