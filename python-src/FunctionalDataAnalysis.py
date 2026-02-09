e8a4ce1c31ad3d71e2d622f672fed3ab76ee6bf2
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2026-02-09 16:14:15 +0530

    ---------------------------------------------------------------------------------------------------------------------------------------- 1650. (THEORY and FEATURE) Functional Data Analysis(Scikit-FDA) implementation - for clustering Functional Data (Curves) across Meterology, Gait Analysis, Handwriting Recognition - related to 148,172,313,445,446,463,1165,1171,1176,1266,1289,1316,1318,1327,1358,1359,1360,1361,1364,1367,1368,1388,1404,1455,1456,1463,1479,1480,1515,1526,1527,1528,1548,1558,1561,1562,1567,1581,1590,1631,1638 and all sections on Correlation between gravitational effects of Mined Celestial N-Body Patterns and terrestrial extreme weather events (or Gravitational effects on climate change), Drone natural obstacle avoidance-Drone autonomous online shopping delivery-Drone Electronic Voting Machine, Artificial Intelligence models for Climate Change-Adaptive Fourier Neural Operator-FourCastNet, Astronomical algorithms, Multifractals, Timeseries analysis, Sequence Mining-Class Association Rules, Global warming,Clausius-Clapeyron equation,Navier-Stokes equation, Handwriting recognition, Gait Analysis, Fraud analytics ---------------------------------------------------------------------------------------------------------------------------------------- 1650.1 New python3.9 file FunctionalDataAnalysis.py has been added to NeuronRain astroinfer repositories that invokes scikit-fda functional k-means clustering for arbitrary dataset of curves instead of usual kmeans clustering over point datasets. 1650.2 Three datasets from skfda (https://fda.readthedocs.io/en/latest/modules/datasets.html) for Gait Analysis of Children (hip and knee angles), AEMET Spanish Weather, HANDWRIT corpus have been imported and clustered by scikit-fda API. 1650.3 Function cluster_functions() wraps over scikit-fda's KMeans fit_and_predict() function, performs curve clustering and plots the clusters - at present scatter plot has been commented due to internal version mistmach between Pandas DataFrame and Scikit-FDA matrix datatypes and only plot() is used to create cluster graphics. 1650.4 Following are logs and cluster plots for FDA KMeans clustering for AEMET,GAIT and HANDWRIT datasets: 	testlogs/FunctionalDataAnalysis1.9February2026.png 	testlogs/FunctionalDataAnalysis2.9February2026.png 	testlogs/FunctionalDataAnalysis3.9February2026.png 	testlogs/FunctionalDataAnalysis.log.9February2026 1650.5 Application of Functional Data Analysis for Gait analysis and Handwriting recognition is especially useful in Cybercrimes-Fraud analytics-Unique Identification alongside Face and biometric authentications - Gait analysis could distinguish AI generated Deepfakes from genuine imagery.
    References:
    ----------
    1650.6 One-way functional ANOVA with real data - GAIT analysis by ANOVA - https://fda.readthedocs.io/en/latest/auto_examples/inference/plot_oneway.html#sphx-glr-auto-examples-inference-plot-oneway-py

---
+++ python-src/FunctionalDataAnalysis.py
@@ -0,0 +1,54 @@
+# -------------------------------------------------------------------------------------------------------
+# NEURONRAIN AI - ASFER - Software for Mining Large Datasets
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------------------------------
+# K.Srinivasan
+# Krishna iResearch - https://www.krishna-iresearch.org/
+# NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+# Personal website(research): https://acadpdrafts.readthedocs.io/en/latest/ 
+# --------------------------------------------------------------------------------------------------------
+
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes as ax 
+from skfda.misc.metrics import l2_distance
+from skfda.ml.clustering import KMeans
+from skfda.datasets import fetch_gait,fetch_aemet,fetch_handwriting
+
+def cluster_functions(Xaxis=None,Yaxis=None,data=None,n_clust=2,n_ini=10):
+    print("Data:",data)
+    n_clusters = n_clust 
+    n_init = n_ini 
+    fda_kmeans = KMeans(
+      n_clusters=n_clusters,
+      n_init=n_init,
+      metric=l2_distance,
+      random_state=0,
+    )
+    fda_clusters = fda_kmeans.fit_predict(data)
+    print("FDA clusters:",fda_clusters)
+    for cluster in range(n_clust):
+        selection = (fda_clusters == cluster)
+        print("selection:",selection)
+        #ax.scatter(data[selection,0],data[selection,1])
+        plt.plot(len(data[0]),len(data[0][0]),selection,'o')
+    plt.show()
+
+if __name__=="__main__":
+    print("-------------Meteorology Dataset FDA Clusters------------")
+    X,_= fetch_aemet(return_X_y=True)
+    cluster_functions(data=X)
+    print("------------Gait Analysis Dataset FDA Clusters-----------")
+    X,_= fetch_gait(return_X_y=True)
+    cluster_functions(data=X)
+    print("-----------Handwriting Dataset FDA Clusters--------------")
+    X,_= fetch_handwriting(return_X_y=True)
+    cluster_functions(data=X)
