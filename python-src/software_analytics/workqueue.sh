4a34f8f0a3f05b4b0ef045ff0733f2bb7a0a489a
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2026-06-15 13:32:00 +0530

    -------------------------------------------------------------------------------------------------- 1670. (THEORY and FEATURE) Software Analytics - Kernel Analytics and Scheduler Analytics - /proc/sched_debug added to workqueue script implementation - related to 561,1191,619,770,906,911,771,1192,1443,1459,1475,1668 and all sections on Program Analysis, AsFer-USBmd-VIRGO-KingCobra FTrace kernel callgraph analysis - Valgrind-Callgrind-KCacheGrind-Flamegraph userspace callgraph analysis, SATURN control flow analysis of VIRGO linux kernel, Software Analytics, Pandemic model of Botnet-Defense adversarial Game theoretic security, Cybercrime analytics, Anti-Malware,Anti-Ransomware,Anti-Theft and Anti-Plagiarism tools, KML, LAKE,Intrusion Prevention-Detection Systems(IDS and IPS),Anomaly Detection - 15 June 2026 -------------------------------------------------------------------------------------------------- 1670.1 python-src/software_analytics/workqueue.sh has been changed to update the /sys/kernel/debug tracing directory structure and include a sched_debug commandline based on ChatGPT prompt to print the Completely Fair Scheduler (CFS) runnable_tasks from cfs_rq.tasks_timeline (kernel private datastructure): 	#mount -t debugfs nodev /sys/kernel/debug 	echo workqueue:workqueue_queue_work > /sys/kernel/debug/tracing/set_event 	cat /sys/kernel/debug/tracing/trace_pipe > workqueue.log 	sudo awk ' /cpu#/ {cpu=$1} /runnable tasks:/ {show=1; next} /^$/ {show=0} show {print cpu, $0} ' /proc/sched_debug > schedrunqueue.log 1670.2 This is in continuation of eBPF updates earlier necessary to gather processes runqueue statistics for Survival Index OS scheduler (in 1668.4) 1670.3 Workqueue and Scheduler logs are at workqueue.log and schedrunqueue.log respectively. ChatGPT prompt for various scheduler runqueue dump alternatives (including eBPF) is cached in ChatGPT_SchedulerRunqueueLinux.pdf

--- python-src/software_analytics/workqueue.sh
+++ python-src/software_analytics/workqueue.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo workqueue:workqueue_queue_work > /sys/kernel/tracing/set_event
-cat /sys/kernel/tracing/trace_pipe > workqueue.log 
+#mount -t debugfs nodev /sys/kernel/debug
+echo workqueue:workqueue_queue_work > /sys/kernel/debug/tracing/set_event
+cat /sys/kernel/debug/tracing/trace_pipe > workqueue.log 
+sudo awk ' /cpu#/ {cpu=$1} /runnable tasks:/ {show=1; next} /^$/ {show=0} show {print cpu, $0} ' /proc/sched_debug > schedrunqueue.log
